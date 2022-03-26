6c9eb910ce1b48e4a63b563d6a35f2bb06cc0af4
dylan <nducus@yahoo.com>
2022-03-26 00:06:11 -0700

    updated sandbox page

--- pages/sandbox/index.tsx
+++ pages/sandbox/index.tsx
@@ -1,5 +1,7 @@
 import type { FC } from 'react';
+import { FaTwitter, FaFacebook, FaEdit } from 'react-icons/fa';
 import { Card } from 'src/components/common/card';
+import Chip from 'src/components/common/chip';
 
 export const SandboxPage: FC = () => {
   return (
@@ -8,17 +10,24 @@ export const SandboxPage: FC = () => {
 
       <h3># Text</h3>
       <div>
-        <h1 className="page-header">Page Header</h1>
-        <div className="text-primary">Primary Text</div>
-        <div className="text-secondary">Secondary Text</div>
+        <div className="text-primary">text-primary</div>
+        <div className="text-secondary">text-secondary</div>
       </div>
 
-      <h3># Button</h3>
+      <h3># Button - WIP</h3>
       <div className="flex flex-row space-x-4">
         <button>Button</button>
       </div>
 
-      <h3># Card</h3>
+      <h3># Chip</h3>
+      <div className="flex flex-row space-x-4">
+        <Chip content="Watch" />
+        <Chip left={<FaEdit />} content="Edit" />
+        <Chip content={<FaTwitter />} />
+        <Chip content={<FaFacebook />} />
+      </div>
+
+      <h3># Card - WIP</h3>
       <div className="flex flex-row space-x-4">
         <Card
           data={{
