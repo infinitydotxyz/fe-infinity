ed7768727c64e93fac97ae1731d405d7c715f32f
dylan <nducus@yahoo.com>
2022-03-29 02:22:48 -0700

    card: added className

--- src/components/common/card.tsx
+++ src/components/common/card.tsx
@@ -1,13 +1,16 @@
 import { CardData } from '@infinityxyz/lib/types/core';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
   data: CardData;
+  className?: string;
 }
 
-export function Card({ data }: Props): JSX.Element {
+export function Card({ data, className }: Props): JSX.Element {
   return (
-    <div className="w-40 h-60 border rounded-lg">
-      <img className="rounded-t-lg" src={data.image ?? ''} alt="card" />
+    <div className={twMerge(`w-40 h-60 border rounded-lg ${className ?? ''}`)}>
+      <img className="rounded-t-lg max-h-80 overflow-hidden" src={data.image ?? ''} alt="card" />
+
       <footer className="text-sm flex items-center justify-between p-1">
         <div className="">{data.price} ETH</div>
         <div className="">Icon</div>
