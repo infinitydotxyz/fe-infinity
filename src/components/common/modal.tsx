ff00a87e44d6cbd8eef8fbd9293ad870bdb7d497
Steve Gehrman <sgehrman@gmail.com>
2022-03-29 15:38:13 -0700

    fix

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -42,7 +42,7 @@ export const Modal = ({ children, onSubmit, okButton = 'OK', title, isOpen, onCl
             leaveTo="opacity-0 scale-95"
           >
             <div className="inline-block w-full max-w-lg p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-              <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+              <Dialog.Title as="h2" className="text-lg font-bold leading-6 text-gray-900 mb-2">
                 {title}
               </Dialog.Title>
 
