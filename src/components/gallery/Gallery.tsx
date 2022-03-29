855854ca5802c6e40a042d1e3f7d4b9046d525ec
dylan <nducus@yahoo.com>
2022-03-29 02:23:35 -0700

    gallery: pagination

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,53 +1,127 @@
 import { BaseCollection, CardData } from '@infinityxyz/lib/types/core';
 import { getSearchFriendlyString } from '@infinityxyz/lib/utils';
-import { useFetch } from 'src/utils/apiUtil';
+import { useEffect, useState } from 'react';
+import { apiGet } from 'src/utils/apiUtil';
+import { ITEMS_PER_PAGE } from 'src/utils/constants';
 import { Button } from '../common';
 import { Card } from '../common/card';
+import { FetchMore } from '../common/fetch-more';
+
+type Asset = {
+  address: string;
+  collectionName: string;
+  id: string;
+  image: string;
+};
+type ListingMetadata = {
+  asset: Asset;
+  basePriceInEth: number;
+};
+type Listing = {
+  id: string;
+  metadata: ListingMetadata;
+};
 
 interface GalleryProps {
   collection: BaseCollection | null;
 }
 
 export function Gallery({ collection }: GalleryProps) {
-  const path = `/collections/${getSearchFriendlyString(collection?.slug)}`;
-  const { result, isLoading, isError, error } = useFetch<BaseCollection>(path, { chainId: '1' });
-  console.log('result, isLoading, isError, error', result, isLoading, isError, error);
+  const [filterShowed, setFilterShowed] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
+  const [data, setData] = useState<CardData[]>([]);
+  const [currentPage, setCurrentPage] = useState(-1);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-  const data: CardData[] = [
-    {
-      id: 'nft1',
-      title: 'NFT 1',
-      price: 1.5,
-      image:
-        'https://media.voguebusiness.com/photos/61b8dfb99ba90ab572dea0bd/3:4/w_1998,h_2664,c_limit/adidas-nft-voguebus-adidas-nft-dec-21-story.jpg'
-    },
-    {
-      id: 'nft2',
-      title: 'NFT 2',
-      price: 2.5,
-      image:
-        'https://media.voguebusiness.com/photos/61b8dfb99ba90ab572dea0bd/3:4/w_1998,h_2664,c_limit/adidas-nft-voguebus-adidas-nft-dec-21-story.jpg'
+  // const path = `/listings`;
+  // const { result, isLoading, isError, error } = useFetch<{ listings: Listing[] }>(path, {
+  //   chainId: '1',
+  //   collectionName: getSearchFriendlyString(collection?.slug),
+  //   offet: 0,
+  //   limit: ITEMS_PER_PAGE
+  // });
+  // console.log('result, isLoading, isError, error', result, isLoading, isError, error);
+  // const listings = result?.listings ?? [];
+
+  // const data: CardData[] = listings.map((listing) => ({
+  //   id: listing.id,
+  //   title: '',
+  //   image: listing.metadata.asset.image
+  // }));
+
+  const fetchData = async () => {
+    setIsFetching(true);
+    const newCurrentPage = currentPage + 1;
+
+    const offset = currentPage > 0 ? currentPage * ITEMS_PER_PAGE : 0;
+    const { result } = await apiGet(`/listings`, {
+      query: {
+        offset, // not "startAfter" because this is not firebase query.
+        limit: ITEMS_PER_PAGE,
+        chainId: '1',
+        collectionName: getSearchFriendlyString(collection?.slug)
+      }
+    });
+
+    const moreData = (result?.listings || []).map((item: Listing) => {
+      return {
+        id: item.id + item.metadata.asset.address + item.metadata.asset.id,
+        title: '',
+        image: item.metadata.asset.image
+      };
+    });
+
+    setIsFetching(false);
+    setData([...data, ...moreData]);
+    setCurrentPage(newCurrentPage);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (currentPage < 0 || data.length < currentPage * ITEMS_PER_PAGE) {
+      return;
     }
-  ];
-  if (isLoading) {
-    return <>Loading...</>;
-  }
+    setDataLoaded(true); // current page's data loaded & rendered.
+  }, [currentPage]);
+
   return (
     <div>
       <header className="text-right">
-        <Button variant="outline">Show Filter</Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            setFilterShowed((flag) => !flag);
+          }}
+        >
+          {filterShowed ? 'Hide' : 'Show'} Filter
+        </Button>
         <Button variant="outline" className="ml-2">
           Sort
         </Button>
       </header>
 
       <div className="flex">
-        <div className="w-1/3">Filter Panel</div>
+        {filterShowed && <div className="w-1/3">Filter Panel</div>}
 
-        <div className="flex space-x-8 mt-6">
-          {data.map((item) => {
-            return <Card key={item.id} data={item} />;
+        <div className="flex flex-wrap mt-6">
+          {data.map((item, idx) => {
+            return <Card key={idx} data={item} className="ml-8 mt-8" />;
           })}
+
+          {dataLoaded && (
+            <FetchMore
+              currentPage={currentPage}
+              data={data}
+              onFetchMore={async () => {
+                console.log('***** onFetchMore', data.length);
+                // setDataLoaded(false);
+                await fetchData();
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
