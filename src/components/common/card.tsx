871309d98a56dd36a5ada0b7ad731c9c6cc690fd
dylan <nducus@yahoo.com>
2022-03-29 09:06:56 -0700

    card styles

--- src/components/common/card.tsx
+++ src/components/common/card.tsx
@@ -8,12 +8,16 @@ interface Props {
 
 export function Card({ data, className }: Props): JSX.Element {
   return (
-    <div className={twMerge(`w-40 h-60 border rounded-lg ${className ?? ''}`)}>
+    <div className={twMerge(`w-40 h-70 border rounded-xl ${className ?? ''}`)}>
       <img className="rounded-t-lg max-h-80 overflow-hidden" src={data.image ?? ''} alt="card" />
+      <div className="p-1">
+        <div className="font-bold">{data.title}</div>
+        <div>{data.tokenId}</div>
+      </div>
 
       <footer className="text-sm flex items-center justify-between p-1">
-        <div className="">{data.price} ETH</div>
-        <div className="">Icon</div>
+        <div className="border rounded-xl flex-1 p-1">Buy</div>
+        <div className="border rounded-xl  p-1">{data.price} ETH</div>
       </footer>
     </div>
   );
