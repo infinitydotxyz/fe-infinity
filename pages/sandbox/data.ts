305fae00ae2980eefaca9db6c654416cdad912c2
dylan <nducus@yahoo.com>
2022-03-30 13:01:53 -0700

    lint

--- pages/sandbox/data.ts
+++ pages/sandbox/data.ts
@@ -16,3 +16,9 @@ export const cardTestData = [
       'https://media.voguebusiness.com/photos/61b8dfb99ba90ab572dea0bd/3:4/w_1998,h_2664,c_limit/adidas-nft-voguebus-adidas-nft-dec-21-story.jpg'
   }
 ];
+
+const testData = {
+  cardTestData
+};
+
+export default testData;
