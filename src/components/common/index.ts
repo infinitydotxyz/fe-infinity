e92642d7dfa76bed9b2bcc8d4b9c3eb03738a70c
dylan <nducus@yahoo.com>
2022-03-30 14:18:34 -0700

    export files

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -6,6 +6,7 @@ export * from 'src/components/common/date-picker';
 export * from 'src/components/common/fetch-more';
 export * from 'src/components/common/loadable';
 export * from 'src/components/common/loading-screen';
+export * from 'src/components/common/rounded-nav';
 export * from 'src/components/common/spacer';
 export * from 'src/components/common/page-box';
 export * from 'src/components/common/text-input';
