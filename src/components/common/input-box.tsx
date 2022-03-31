1c845b2c19c734bc008afde0613ea6460810e892
John Carter <johncarter7061@gmail.com>
2022-03-31 07:49:05 -0400

    fix lint issue

--- src/components/common/input-box.tsx
+++ src/components/common/input-box.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from 'react';
 
 interface Props {
-  label: string;
+  label?: string;
   children: ReactNode;
 }
 
 export function InputBox({ label, children }: Props): JSX.Element {
   return (
     <div className="py-2 pl-6 pr-2 mb-1 outline outline-1 outline-slate-300 rounded-2xl ">
-      <label className="block text-xs font-medium text-gray-700">{label}</label>
+      {label && <label className="block text-xs font-medium text-gray-700">{label}</label>}
       <div className="mt-1 flex items-center">{children}</div>
     </div>
   );
