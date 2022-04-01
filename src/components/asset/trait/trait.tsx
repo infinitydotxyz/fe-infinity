b8f2440b2c3837c61a8a5ecbc9a4b9b843580c38
John Carter <johncarter7061@gmail.com>
2022-04-01 14:47:43 -0400

    Fix padding

--- src/components/asset/trait/trait.tsx
+++ src/components/asset/trait/trait.tsx
@@ -13,7 +13,7 @@ export const Trait: FC<Props> = ({ trait, description }) => {
         <p className="text-center text-sm text-theme-light-800 mb-1 font-body">{trait.trait_type}</p>
         <p className="text-center text-sm font-body font-semibold">{trait.value}</p>
       </div>
-      <p className="text-center text-xs text-theme-light-800 bg-theme-light-300 rounded-3xl py-1 mt-3.5 lg:mt-5 font-body tracking-tighter px-2">
+      <p className="text-center text-xs text-theme-light-800 bg-theme-light-300 rounded-3xl py-1 mt-3 lg:mt-4 font-body tracking-tighter px-2">
         {description}
       </p>
     </div>
