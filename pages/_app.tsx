cb0f3d006340c23ac3c4da7212d63e6fd71db16c
Ayush Sharma <code.ayush@gmail.com>
2022-04-01 19:43:05 +0530

    Fix _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,54 +1,62 @@
-import 'src/settings/theme/globals.scss';
+import React, { FunctionComponent, memo, StrictMode, useEffect } from 'react';
 import type { AppProps } from 'next/app';
-import React, { ComponentType, createElement, FunctionComponent, memo, StrictMode, useEffect } from 'react';
+import 'src/settings/theme/globals.scss';
+import { useRouter } from 'next/router';
+import LogRocket from 'logrocket';
 import * as gtag from 'lib/ga/gtag';
-const isProduction = process.env.NODE_ENV === 'production';
-import { BrowserRouter } from 'react-router-dom';
 import { AppContextProvider } from 'src/utils/context/AppContext';
 import { FilterContextProvider } from 'src/utils/context/FilterContext';
-import { useRouter } from 'next/router';
-import { isLocalhost } from 'src/utils/commonUtils';
-import LogRocket from 'logrocket';
+import { isLocalhost, isServer } from 'src/utils/commonUtils';
 import { OrderContextProvider } from 'src/utils/context/OrderContext';
 
-const providers: readonly ComponentType[] = [BrowserRouter];
-
-const PageComponent: FunctionComponent<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
-
-const MemoizedComponent = memo(PageComponent, (p, n) => p.Component === n.Component && p.pageProps === n.pageProps);
+/*
+  ======================================
+    Following constants are just memoized
+    Page component (optimizations) and variable describing
+    whether the environment is production
+    or localhost (required for ga tracking).
+  ======================================
+*/
+const Page: FunctionComponent<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const Memoized = memo(Page, (p, n) => p.Component === n.Component && p.pageProps === n.pageProps);
+const isProduction = process.env.NODE_ENV === 'production';
 
 if (!isLocalhost()) {
   LogRocket.init('0pu9ak/nftco');
 }
 
 const App: FunctionComponent<AppProps> = (props) => {
+  /*
+    ======================================
+      For every route change in production,
+      we inject google analytics tracker.
+    ======================================
+  */
   const router = useRouter();
-
   useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      /* invoke analytics function only for production */
-      if (isProduction) {
-        gtag.pageview(url);
-      }
-    };
+    const handleRouteChange = (url: URL) => (isProduction ? gtag.pageview(url) : null);
     router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
   }, [router.events]);
 
-  return typeof window === 'undefined' ? null : (
+  /*
+    ======================================
+      Minor optimizations:
+      - Disable SSR (check if window is defined).
+      - Memoize Page component.
+      Additionally: Wrap up the entire thing
+      within context providers.
+    ======================================
+  */
+  return isServer() ? null : (
     <StrictMode>
-      {providers.reduceRight(
-        (children, provider) => createElement(provider, undefined, children),
-        <AppContextProvider>
-          <FilterContextProvider>
-            <OrderContextProvider>
-              <MemoizedComponent {...props} />
-            </OrderContextProvider>
-          </FilterContextProvider>
-        </AppContextProvider>
-      )}
+      <AppContextProvider>
+        <FilterContextProvider>
+          <OrderContextProvider>
+            <Memoized {...props} />
+          </OrderContextProvider>
+        </FilterContextProvider>
+      </AppContextProvider>
     </StrictMode>
   );
 };
