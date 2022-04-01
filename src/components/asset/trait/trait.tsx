e5e06c46fdb5946864d5fb480c360684896c48a6
John Carter <johncarter7061@gmail.com>
2022-04-01 14:48:24 -0400

    Fix: Trait value font style

--- src/components/asset/trait/trait.tsx
+++ src/components/asset/trait/trait.tsx
@@ -11,7 +11,7 @@ export const Trait: FC<Props> = ({ trait, description }) => {
     <div className="border border-gray-400 rounded-3xl px-5 py-5 flex flex-col justify-between">
       <div>
         <p className="text-center text-sm text-theme-light-800 mb-1 font-body">{trait.trait_type}</p>
-        <p className="text-center text-sm font-body font-semibold">{trait.value}</p>
+        <p className="text-center text-sm font-body font-bold">{trait.value}</p>
       </div>
       <p className="text-center text-xs text-theme-light-800 bg-theme-light-300 rounded-3xl py-1 mt-3 lg:mt-4 font-body tracking-tighter px-2">
         {description}
