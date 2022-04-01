e1bdc641316a67cc62ecf59cfe2ec7ba2f956cb8
John Carter <johncarter7061@gmail.com>
2022-04-01 09:06:37 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import 'src/settings/theme/globals.scss';
 import type { AppProps } from 'next/app';
-import React, { FunctionComponent, StrictMode, useEffect } from 'react';
+import React, { ComponentType, createElement, FunctionComponent, memo, StrictMode, useEffect } from 'react';
 import * as gtag from 'lib/ga/gtag';
 const isProduction = process.env.NODE_ENV === 'production';
+import { BrowserRouter } from 'react-router-dom';
 import { AppContextProvider } from 'src/utils/context/AppContext';
 import { FilterContextProvider } from 'src/utils/context/FilterContext';
 import { useRouter } from 'next/router';
@@ -10,11 +11,17 @@ import { isLocalhost, isServer } from 'src/utils/commonUtils';
 import LogRocket from 'logrocket';
 import { OrderContextProvider } from 'src/utils/context/OrderContext';
 
+const providers: readonly ComponentType[] = [BrowserRouter];
+
+const PageComponent: FunctionComponent<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+const MemoizedComponent = memo(PageComponent, (p, n) => p.Component === n.Component && p.pageProps === n.pageProps);
+
 if (!isLocalhost()) {
   LogRocket.init('0pu9ak/nftco');
 }
 
-const App: FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps) => {
+const App: FunctionComponent<AppProps> = (props) => {
   const router = useRouter();
 
   useEffect(() => {
@@ -32,13 +39,16 @@ const App: FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps) =>
 
   return isServer() ? null : (
     <StrictMode>
-      <AppContextProvider>
-        <FilterContextProvider>
-          <OrderContextProvider>
-            <Component {...pageProps} />
-          </OrderContextProvider>
-        </FilterContextProvider>
-      </AppContextProvider>
+      {providers.reduceRight(
+        (children, provider) => createElement(provider, undefined, children),
+        <AppContextProvider>
+          <FilterContextProvider>
+            <OrderContextProvider>
+              <MemoizedComponent {...props} />
+            </OrderContextProvider>
+          </FilterContextProvider>
+        </AppContextProvider>
+      )}
     </StrictMode>
   );
 };
