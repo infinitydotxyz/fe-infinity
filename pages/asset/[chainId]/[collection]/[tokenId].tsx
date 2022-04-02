dc77f43e88333d68cf251c578549528bda67ea55
Steve Gehrman <sgehrman@gmail.com>
2022-04-01 21:54:43 -0700

    removed unused icon

--- pages/asset/[chainId]/[collection]/[tokenId].tsx
+++ pages/asset/[chainId]/[collection]/[tokenId].tsx
@@ -15,7 +15,7 @@ import {
 } from 'src/components/asset';
 
 import BlueCheckSvg from 'src/images/blue-check.svg';
-// import TransferSvg from 'src/images/transfer.svg';
+// import {HiOutlineSwitchHorizontal} from 'react-icons';
 
 import { NextPageContext } from 'next';
 
