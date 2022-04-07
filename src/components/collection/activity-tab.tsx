8e14247fef1d7d593c2e887b572024b1f360ce37
dylan <nducus@yahoo.com>
2022-04-07 16:36:00 -0700

    chart with dailyStats

--- src/components/collection/activity-tab.tsx
+++ src/components/collection/activity-tab.tsx
@@ -1,60 +1,25 @@
-import { CollectionStats } from '@infinityxyz/lib/types/core';
+import { BaseCollection, CollectionStats } from '@infinityxyz/lib/types/core';
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { useFetch } from 'src/utils';
 
 type StatsData = {
   data: CollectionStats[];
 };
 
 interface ActivityTabProps {
+  collection: BaseCollection;
   dailyStats?: StatsData | null;
   weeklyStats?: StatsData | null;
 }
 
-export function ActivityTab({ weeklyStats }: ActivityTabProps) {
-  const data = [
-    {
-      name: 'Apr 1',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400
-    },
-    {
-      name: 'Apr 2',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210
-    },
-    {
-      name: 'Apr 3',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290
-    },
-    {
-      name: 'Apr 4',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000
-    },
-    {
-      name: 'Apr 5',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181
-    },
-    {
-      name: 'Apr 6',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500
-    },
-    {
-      name: 'Apr 7',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100
-    }
-  ];
+export function ActivityTab({ dailyStats, weeklyStats }: ActivityTabProps) {
+  const data = dailyStats?.data.map((item) => {
+    return {
+      name: new Date(item.timestamp).toLocaleDateString(),
+      average: item.avgPrice
+    };
+  });
+
   return (
     <>
       <div className="text-3xl mb-6">Activity trend</div>
@@ -91,7 +56,7 @@ export function ActivityTab({ weeklyStats }: ActivityTabProps) {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#0000ff" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="average" stroke="#0000ff" activeDot={{ r: 8 }} />
           {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
         </LineChart>
       </div>
