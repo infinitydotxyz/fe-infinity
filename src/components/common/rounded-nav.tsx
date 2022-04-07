f93ded12ee419b8bcc000d9395427e431aa1c89d
dylan <nducus@yahoo.com>
2022-04-06 20:36:04 -0700

    RoundedNav styles

--- src/components/common/rounded-nav.tsx
+++ src/components/common/rounded-nav.tsx
@@ -21,32 +21,34 @@ export function RoundedNav({ items, defaultIndex, onChange, className }: Props) 
   }, [defaultIndex]);
 
   return (
-    <div
-      className={twMerge(`flex justify-between space-x-4 bg-gray-100 rounded-2xl ${className ?? ''}`)}
-      aria-label="Tabs"
-    >
-      {items.map((item, index: number) => {
-        const isActive = index === activeIndex;
-        const activeCx = isActive ? 'bg-black text-white' : '';
-        return (
-          <a
-            key={item.title}
-            href=""
-            className={twMerge(
-              `px-10 py-2 font-medium font-heading rounded-3xl text-sm bg-gray-100 text-secondary ${activeCx}`
-            )}
-            onClick={(e) => {
-              e.preventDefault();
-              setActiveIndex(index);
-              if (onChange) {
-                onChange(index);
-              }
-            }}
-          >
-            {item.title}
-          </a>
-        );
-      })}
+    <div className="flex">
+      <div
+        className={twMerge(`flex justify-between space-x-1 bg-gray-100 rounded-2xl ${className ?? ''}`)}
+        aria-label="Tabs"
+      >
+        {items.map((item, index: number) => {
+          const isActive = index === activeIndex;
+          const activeCx = isActive ? 'bg-black text-white' : '';
+          return (
+            <a
+              key={item.title}
+              href=""
+              className={twMerge(
+                `px-10 py-2 font-medium font-heading rounded-3xl text-sm bg-gray-100 text-secondary ${activeCx}`
+              )}
+              onClick={(e) => {
+                e.preventDefault();
+                setActiveIndex(index);
+                if (onChange) {
+                  onChange(index);
+                }
+              }}
+            >
+              {item.title}
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 }
