c3fc28671a7f3c074d033415d2295a54e80bab3a
John Carter <johncarter7061@gmail.com>
2022-04-11 14:42:48 -0400

    Fix layout

--- pages/asset/[chainId]/[collection]/[tokenId].tsx
+++ pages/asset/[chainId]/[collection]/[tokenId].tsx
@@ -41,7 +41,7 @@ const AssetDetail: FunctionComponent<AssetDetailProps> = ({ token, collection })
       : token.metadata.name || collection.metadata.name || 'No Name';
   return (
     <PageBox title={assetName} hideTitle>
-      <div className="pb-4 sm:flex">
+      <div className="sm:flex">
         <div className="min-h-12 w-80 mx-auto sm:w-96 md:w-96 lg:w-144 sm:mr-6 md:mr-8 lg:mr-12 mb-4">
           <img
             className="rounded-3xl w-80 mx-auto sm:w-96 md:w-96 lg:w-144"
