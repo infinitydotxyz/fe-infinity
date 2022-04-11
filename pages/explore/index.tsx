1faec73a699d9f765917dda7bdc8c9ed29be4874
dylan <nducus@yahoo.com>
2022-04-10 20:17:43 -0700

    explore: use Card

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
-import { PageBox } from 'src/components/common';
+import { Card, PageBox } from 'src/components/common';
 import { FetchMore } from 'src/components/common';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { SearchBox } from 'src/components/filter/search-box';
 //import { NextPageContext } from 'next';
 //import { apiGet } from 'src/utils';
 //import { Collection } from '@infinityxyz/lib/types/core';
 //import { GalleryBox } from 'src/components/gallery/gallery-box';
-
-// get image ids here https://picsum.photos/images
-const BLANK_IMAGE_URL =
-  'https://imageio.forbes.com/specials-images/imageserve/6170e01f8d7639b95a7f2eeb/Sotheby-s-NFT-Natively-Digital-1-2-sale-Bored-Ape-Yacht-Club--8817-by-Yuga-Labs/0x0.png?fit=bounds&format=png&width=960';
 
 const sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
@@ -30,7 +26,7 @@ const ExplorePage: React.FC = () => {
 
       <h1 className="text-heading text-3xl">Collections</h1>
       <div className="flex justify-between flex-wrap -mx-4">
-        {res.map((val, key) => (
+        {/* {res.map((val, key) => (
           <div key={key} className="w-full sm:w-72 lg:w-80 p-4">
             <Link href={`/asset/1/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/8880`}>
               <a href={`/asset/1/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/8880`}>
@@ -41,7 +37,22 @@ const ExplorePage: React.FC = () => {
               </a>
             </Link>
           </div>
-        ))}
+        ))} */}
+        {res.map((val, key) => {
+          const dt = {
+            id: 'nft1',
+            title: 'NFT 1',
+            tokenId: 'Token1',
+            price: 1.5,
+            image:
+              'https://media.voguebusiness.com/photos/61b8dfb99ba90ab572dea0bd/3:4/w_1998,h_2664,c_limit/adidas-nft-voguebus-adidas-nft-dec-21-story.jpg'
+          };
+          return (
+            <React.Fragment key={key}>
+              <Card data={dt} onClick={console.log} className="mt-8 ml-8" />
+            </React.Fragment>
+          );
+        })}
         <FetchMore onFetchMore={handleFetchMore} data={data} currentPage={page} />
       </div>
     </PageBox>
