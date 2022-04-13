b6a3874faf2b9800cd03fd089e95c18bccbea3a0
Sleeyax <yourd3veloper@gmail.com>
2022-04-13 21:53:49 +0200

    create base container

--- pages/collection/[name]/edit/index.tsx
+++ pages/collection/[name]/edit/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Button } from 'src/components/common';
+import logo from 'src/images/logo-mini-new.svg';
 
 export default function EditCollectionPage() {
-  return <span>edit collection</span>;
+  return (
+    <div className="transition w-[100vw] h-[100vh] overflow-y-auto">
+      <header className="flex justify-between p-5">
+        <img alt="logo" src={logo.src} width={logo.width} />
+        <div className="flex flex-row space-x-2">
+          <Button variant="outline">Cancel</Button>
+          <Button>Save</Button>
+        </div>
+      </header>
+    </div>
+  );
 }
