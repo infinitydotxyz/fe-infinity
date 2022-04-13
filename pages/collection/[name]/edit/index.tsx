67104aea524ef9400d6650092ad38046474f2195
Sleeyax <yourd3veloper@gmail.com>
2022-04-13 23:33:56 +0200

    add heading component, edit collection section (UI)

--- pages/collection/[name]/edit/index.tsx
+++ pages/collection/[name]/edit/index.tsx
@@ -2,9 +2,14 @@ import { BaseCollection } from '@infinityxyz/lib/types/core';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { AvatarImage } from 'src/components/collection/avatar-image';
-import { Button } from 'src/components/common';
+import { Button, InputBox } from 'src/components/common';
+import { Heading } from 'src/components/common/heading';
 import logo from 'src/images/logo-mini-new.svg';
 import { useFetch } from 'src/utils';
+
+const spaces = {
+  article: 'space-y-5'
+};
 
 export default function EditCollectionPage() {
   const router = useRouter();
@@ -24,13 +29,25 @@ export default function EditCollectionPage() {
           <Button>Save</Button>
         </nav>
       </header>
-      <main className="flex flex-col my-4 mx-auto w-144">
+      <main className="flex flex-col my-4 mx-auto w-144 space-y-5">
         <article className="flex flex-row items-center">
           <AvatarImage url={collection?.metadata.profileImage} size="large" />
           <div className="flex flex-col space-y-2 ml-2">
             <Button>Upload</Button>
             <Button variant="outline">Delete</Button>
           </div>
+        </article>
+
+        <article className={spaces.article}>
+          <Heading as="h3">Edit collection</Heading>
+          <InputBox label="Collection name">Vortex</InputBox>
+          <InputBox label="Description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </InputBox>
         </article>
       </main>
     </div>
---
+++ src/components/common/heading.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export type HeadingType = 'h6' | 'h5' | 'h4' | 'h3' | 'h2' | 'h1';
+
+const sizes: Record<HeadingType, string> = {
+  h1: 'text-7xl',
+  h2: 'text-6xl',
+  h3: 'text-5xl',
+  h4: 'text-4xl',
+  h5: 'text-3xl',
+  h6: 'text-2xl'
+};
+
+export interface Props {
+  as?: HeadingType;
+  className?: string;
+}
+
+export const Heading: React.FC<Props> = ({ as = 'h1', children, className }) => {
+  const Component = as;
+  return <Component className={twMerge(sizes[as], className)}>{children}</Component>;
+};
