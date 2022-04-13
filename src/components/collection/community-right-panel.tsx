020a76e076cf3f8c8d7f2109b733671075d91e50
John Carter <johncarter7061@gmail.com>
2022-04-13 07:25:06 -0400

    Update style

--- src/components/collection/community-right-panel.tsx
+++ src/components/collection/community-right-panel.tsx
@@ -1,12 +1,14 @@
 import { FunctionComponent } from 'react';
 import { VotedStatus } from './voted-status';
 import { TwitterSupporterList } from './twitter-supporter-list';
+import { TopHolderList } from './top-holder-list';
 
 const CommunityRightPanel: FunctionComponent = () => {
   return (
     <div>
       <VotedStatus />
       <TwitterSupporterList />
+      <TopHolderList />
     </div>
   );
 };
