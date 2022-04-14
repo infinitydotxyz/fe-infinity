e05f15e8838944b0e30c57dc71c2e2841981cab2
dylan <nducus@yahoo.com>
2022-04-14 15:52:20 -0700

    follow button icon

--- src/components/collection/stats-chips.tsx
+++ src/components/collection/stats-chips.tsx
@@ -1,5 +1,6 @@
 import { BaseCollection, CollectionStats } from '@infinityxyz/lib/types/core';
-import { FaCaretDown, FaCaretUp, FaDiscord, FaTwitter } from 'react-icons/fa';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { FaCaretDown, FaCaretUp, FaDiscord, FaInstagram, FaTwitter } from 'react-icons/fa';
 import { apiPost } from 'src/utils';
 import { useAppContext } from 'src/utils/context/AppContext';
 import { Chip } from '../common';
@@ -35,56 +36,77 @@ export function StatsChips({ collection, weeklyStatsData }: Props) {
 
   return (
     <div className="flex flex-row space-x-1">
-      <Chip content="+ Follow" onClick={onClickFollow} />
+      <Chip
+        content={
+          <span className="flex items-center">
+            <AiOutlinePlus className="mr-1" /> Follow
+          </span>
+        }
+        onClick={onClickFollow}
+      />
       <Chip content="Edit" />
-      <Chip
-        left={<FaTwitter />}
-        onClick={() => window.open(collection?.metadata.links.twitter)}
-        content={
-          <span className="flex items-center">
-            {lastWeeklyStats?.twitterFollowers?.toLocaleString() ?? '—'}
-            {lastWeeklyStats?.twitterFollowersPercentChange && twitterChangePct !== '0.00' ? (
-              <>
-                {(lastWeeklyStats?.twitterFollowersPercentChange ?? 0) < 0 ? (
-                  <span className="ml-2 py-1 px-2 rounded-xl bg-red-500 text-white text-xs flex items-center">
-                    <FaCaretDown className="mr-1" /> {twitterChangePct}%
-                  </span>
-                ) : (
-                  <span className="ml-2 py-1 px-2 rounded-xl bg-green-500 text-white text-xs flex items-center">
-                    <FaCaretUp className="mr-1" /> {twitterChangePct}%
-                  </span>
-                )}
-              </>
-            ) : (
-              ''
-            )}
-          </span>
-        }
-      />
-      <Chip
-        left={<FaDiscord />}
-        onClick={() => window.open(collection?.metadata.links.discord)}
-        content={
-          <span className="flex items-center">
-            {lastWeeklyStats?.discordFollowers?.toLocaleString() ?? '—'}
-            {lastWeeklyStats?.discordFollowersPercentChange && discordChangePct !== '0.00' ? (
-              <>
-                {(lastWeeklyStats?.discordFollowersPercentChange ?? 0) < 0 ? (
-                  <span className="ml-2 py-1 px-2 rounded-xl bg-red-500 text-white text-xs flex items-center">
-                    <FaCaretDown className="mr-1" /> {discordChangePct}%
-                  </span>
-                ) : (
-                  <span className="ml-2 py-1 px-2 rounded-xl bg-green-500 text-white text-xs flex items-center">
-                    <FaCaretUp className="mr-1" /> {discordChangePct}%
-                  </span>
-                )}
-              </>
-            ) : (
-              ''
-            )}
-          </span>
-        }
-      />
+
+      {collection?.metadata.links.twitter && (
+        <Chip
+          left={<FaTwitter />}
+          onClick={() => window.open(collection?.metadata.links.twitter)}
+          content={
+            <span className="flex items-center">
+              {lastWeeklyStats?.twitterFollowers?.toLocaleString() ?? '—'}
+              {lastWeeklyStats?.twitterFollowersPercentChange && twitterChangePct !== '0.00' ? (
+                <>
+                  {(lastWeeklyStats?.twitterFollowersPercentChange ?? 0) < 0 ? (
+                    <span className="ml-2 py-1 px-2 rounded-xl bg-red-500 text-white text-xs flex items-center">
+                      <FaCaretDown className="mr-1" /> {twitterChangePct}%
+                    </span>
+                  ) : (
+                    <span className="ml-2 py-1 px-2 rounded-xl bg-green-500 text-white text-xs flex items-center">
+                      <FaCaretUp className="mr-1" /> {twitterChangePct}%
+                    </span>
+                  )}
+                </>
+              ) : (
+                ''
+              )}
+            </span>
+          }
+        />
+      )}
+
+      {collection?.metadata.links.discord && (
+        <Chip
+          left={<FaDiscord />}
+          onClick={() => window.open(collection?.metadata.links.discord)}
+          content={
+            <span className="flex items-center">
+              {lastWeeklyStats?.discordFollowers?.toLocaleString() ?? '—'}
+              {lastWeeklyStats?.discordFollowersPercentChange && discordChangePct !== '0.00' ? (
+                <>
+                  {(lastWeeklyStats?.discordFollowersPercentChange ?? 0) < 0 ? (
+                    <span className="ml-2 py-1 px-2 rounded-xl bg-red-500 text-white text-xs flex items-center">
+                      <FaCaretDown className="mr-1" /> {discordChangePct}%
+                    </span>
+                  ) : (
+                    <span className="ml-2 py-1 px-2 rounded-xl bg-green-500 text-white text-xs flex items-center">
+                      <FaCaretUp className="mr-1" /> {discordChangePct}%
+                    </span>
+                  )}
+                </>
+              ) : (
+                ''
+              )}
+            </span>
+          }
+        />
+      )}
+
+      {collection?.metadata.links.instagram && (
+        <Chip
+          content={<FaInstagram className="text-lg" />}
+          onClick={() => window.open(collection?.metadata.links.instagram)}
+          className="p-0"
+        />
+      )}
 
       <Toaster />
     </div>
