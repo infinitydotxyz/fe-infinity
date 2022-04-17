37bf0dde29b250ac4a2d3363cb448dab919c5080
Adi Kancherla <findadit@gmail.com>
2022-04-17 16:33:01 +0530

    fix

--- src/components/market/orderbook-list/orderbook_item.tsx
+++ src/components/market/orderbook-list/orderbook_item.tsx
@@ -43,7 +43,8 @@ export const OrderbookItem = ({ title, content, nameItem, order }: Props4): JSX.
 
   if (nameItem) {
     const nFts = [];
-    const nfts = order.nftsWithMetadata;
+    // todo: temp fix
+    const nfts = order.nftsWithMetadata ?? order.nfts ?? [];
     for (const n of nfts) {
       if (n.tokens.length > 0) {
         for (const t of n.tokens) {
