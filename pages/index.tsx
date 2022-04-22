937946ed79a50b8aec9aded7241cfd642d2bb115
dylan <nducus@yahoo.com>
2022-04-22 10:52:03 -0700

    removed the dummy nav page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,37 @@
-import { useRouter } from 'next/router';
-import type { FC } from 'react';
-import { Button } from 'src/components/common';
+import React, { useState, FunctionComponent, useCallback } from 'react';
 import { Layout } from 'src/components/common/layout';
+import debounce from 'lodash/debounce';
+import { CollectionGrid } from 'src/components/common/collection-grid';
 
-export const Home: FC = () => {
-  const router = useRouter();
+const HomePage: FunctionComponent = () => {
+  const [query, setQuery] = useState('');
+  const [debouncedQuery, setDebouncedQuery] = useState('');
+
+  const handleChange = async (value: string) => {
+    setQuery(value);
+    setQueryDebounced(value);
+  };
+
+  // must use useCallback or it doesn't work
+  const setQueryDebounced = useCallback(
+    debounce((value: string) => {
+      setDebouncedQuery(value);
+    }, 300),
+    []
+  );
 
   return (
-    <Layout title="Home" className="w-full h-full grid place-content-center">
-      <div className="flex flex-col space-y-4 items-center">
-        <Button
-          onClick={() => {
-            router.push('/connect');
-          }}
-        >
-          Connect
-        </Button>
+    <Layout title="Explore" padded>
+      <h1 className="text-2xl font-body font-bold mb-3">All collections</h1>
+      <input
+        value={query}
+        className="w-full border border-gray-500 focus:ring-0 py-2 my-2 pl-3 pr-10 text-lg leading-5 text-gray-900 "
+        onChange={(event) => handleChange(event.target.value)}
+      />
 
-        <Button
-          onClick={() => {
-            router.push('/market');
-          }}
-        >
-          Market
-        </Button>
-
-        <Button
-          onClick={() => {
-            router.push('/collection/boredapeyachtclub');
-          }}
-        >
-          Collection
-        </Button>
-
-        <Button
-          onClick={() => {
-            router.push('/home');
-          }}
-        >
-          Homepage Feed
-        </Button>
-
-        <Button
-          onClick={() => {
-            router.push('/analytics/trending/hourly');
-          }}
-        >
-          Analytics
-        </Button>
-
-        <Button
-          onClick={() => {
-            router.push('/sandbox');
-          }}
-        >
-          SandBox
-        </Button>
-        <Button
-          onClick={() => {
-            router.push('/account');
-          }}
-        >
-          Account
-        </Button>
-        <Button
-          onClick={() => {
-            router.push('/asset/1/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/8880');
-          }}
-        >
-          Asset Detail
-        </Button>
-        <Button
-          onClick={() => {
-            router.push('/explore-collections');
-          }}
-        >
-          All Collections
-        </Button>
-      </div>
+      <CollectionGrid query={debouncedQuery} onClick={(data) => console.log(data)} />
     </Layout>
   );
 };
 
-export default Home;
+export default HomePage;
