e1bfd60150385fadd99c3dbd918cf6c2bb67af88
Steve Gehrman <sgehrman@gmail.com>
2022-04-29 06:42:42 -0700

    fix

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-// import { GiHamburgerMenu } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { IoMdArrowDropdown } from 'react-icons/io';
 import { Menu } from '@headlessui/react';
 import { SVG, SearchInput, ConnectButton, Spacer, CustomMenuItem, pageStyles } from 'src/components/common';
@@ -71,11 +71,47 @@ export const Navbar = () => {
     }
   };
 
-  return (
+  const mobileMenu = (
+    <div className="p-4 w-auto">
+      <Menu>
+        <Menu.Button>
+          <GiHamburgerMenu />
+        </Menu.Button>
+        <Menu.Items
+          className={twMerge(
+            `absolute mt-2 p-4 w-72 origin-top-right divide-y divide-gray-100 rounded-3xl z-50
+            border border-gray-200 bg-white shadow-2xl outline-none`
+          )}
+        >
+          {content?.buttons?.items?.map((item, i) => (
+            <React.Fragment key={i}>
+              {item.type === 'link' && (
+                <CustomMenuItem key={i} onClick={() => router.push(item.props?.href ?? '')}>
+                  {item?.label}
+                </CustomMenuItem>
+              )}
+              {item.type === 'dropdown' && (
+                <>
+                  <hr className="my-1" />
+                  {item?.menu?.map((x, j) => (
+                    <CustomMenuItem key={j} onClick={x.onClick}>
+                      {x?.label}
+                    </CustomMenuItem>
+                  ))}
+                </>
+              )}
+            </React.Fragment>
+          ))}
+        </Menu.Items>
+      </Menu>
+    </div>
+  );
+
+  const desktopMenu = (
     <div className="w-full z-50 sticky top-0 bg-white bg-opacity-70 glass">
       <div className={`${pageStyles} flex space-x-6 items-center py-6 w-full`}>
         <div onClick={() => router.push('/')}>
-          <SVG.logo className="h-16 justify-self-start text-center hover:cursor-pointer"></SVG.logo>
+          <SVG.logo className="h-8 justify-self-start text-center hover:cursor-pointer"></SVG.logo>
         </div>
         <Spacer />
         <div>
@@ -84,13 +120,6 @@ export const Navbar = () => {
 
         {content?.buttons?.items?.map((item, i) => (
           <React.Fragment key={i}>
-            {item.type === 'external' && (
-              <div>
-                <Link passHref href={item?.props?.href ? item.props.href : ''}>
-                  {item?.label}
-                </Link>
-              </div>
-            )}
             {item.type === 'link' && (
               <div>
                 <Link passHref href={item?.props?.href ? item.props.href : ''}>
@@ -129,55 +158,11 @@ export const Navbar = () => {
       </div>
     </div>
   );
-};
 
-// <div {...styles?.actions?.items?.mobile}>
-// <Menu as="div" {...styles?.actions?.item?.menu?.container}>
-//   <Menu.Button {...styles?.actions?.item?.menu?.button}>
-//     <GiHamburgerMenu />
-//   </Menu.Button>
-//   <Menu.Items {...styles?.actions?.item?.menu?.items}>
-//     {content?.buttons?.items?.map((item, i) => (
-//       <React.Fragment key={i}>
-//         {item.type === 'external' && (
-//           <Link passHref href={item?.props?.href ? item.props.href : ''}>
-//             <Menu.Item as="button" {...styles?.actions?.item?.menu?.item}>
-//               {item?.label}
-//             </Menu.Item>
-//           </Link>
-//         )}
-//         {item.type === 'link' && (
-//           <Link passHref href={item?.props?.href ? item.props.href : ''}>
-//             <Menu.Item as="button" {...styles?.actions?.item?.menu?.item}>
-//               {item?.label}
-//             </Menu.Item>
-//           </Link>
-//         )}
-//         {item.type === 'dropdown' && (
-//           <>
-//             <hr className="my-1" />
-//             {item?.menu?.map((x, j) => (
-//               <React.Fragment key={j}>
-//                 {x.type === 'external' && (
-//                   <Link passHref href={x?.props?.href ? x.props.href : ''}>
-//                     <Menu.Item as="button" {...styles?.actions?.item?.menu?.item}>
-//                       {x?.label}
-//                     </Menu.Item>
-//                   </Link>
-//                 )}
-//                 {x.type === 'link' && (
-//                   <Link passHref href={x?.props?.href ? x.props.href : ''}>
-//                     <Menu.Item as="button" {...styles?.actions?.item?.menu?.item}>
-//                       {x?.label}
-//                     </Menu.Item>
-//                   </Link>
-//                 )}
-//               </React.Fragment>
-//             ))}
-//           </>
-//         )}
-//       </React.Fragment>
-//     ))}
-//   </Menu.Items>
-// </Menu>
-// </div>
+  return (
+    <>
+      <div className="desktop:visible tabloid:hidden">{desktopMenu}</div>
+      <div className="desktop:hidden tabloid:visible">{mobileMenu}</div>
+    </>
+  );
+};
