6383dfc60f20d7ab882eb02e47b05e9f17278903
Steve Gehrman <sgehrman@gmail.com>
2022-04-30 02:36:48 -0700

    fix

--- src/components/common/toaster.tsx
+++ src/components/common/toaster.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 import toast, { Toast, Toaster as RHTRoaster } from 'react-hot-toast';
 import { IoMdCheckmarkCircleOutline } from 'react-icons/io';
 import { MdErrorOutline, MdWarningAmber } from 'react-icons/md';
+import { XIcon } from '@heroicons/react/outline';
+import { Button } from './button';
 
 export const Toaster = () => {
   return <RHTRoaster />;
@@ -31,27 +33,15 @@ const ToasterTemplate = ({
                 {content && <p className="mt-1 text-sm text-gray-500">{content}</p>}
               </div>
               <div className="ml-4 flex-shrink-0 flex">
-                <button
+                <Button
+                  variant="round"
+                  size="plain"
                   onClick={() => {
                     toast.remove(t.id);
                   }}
-                  className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  <span className="sr-only">Close</span>
-                  <svg
-                    className="h-5 w-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </button>
+                  <XIcon className="h-5 w-5" aria-hidden="true" />
+                </Button>
               </div>
             </div>
           </div>
