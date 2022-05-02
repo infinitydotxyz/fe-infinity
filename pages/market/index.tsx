a6c51b8c1a36a8bb1adc5fe7edc386b398ebcefa
Jason Ellington <jason.t.ellington@gmail.com>
2022-05-02 16:59:59 -0400

    feat: added sort functionality

--- pages/market/index.tsx
+++ pages/market/index.tsx
@@ -1,14 +1,13 @@
-import { Button, ToggleTab, useToggleTab, Spacer, Dropdown, PageBox } from 'src/components/common';
+import { Button, ToggleTab, useToggleTab, Spacer, PageBox } from 'src/components/common';
 import { OrderDrawer } from 'src/components/market';
 import { useOrderContext } from 'src/utils/context/OrderContext';
 import { FaShoppingBag } from 'react-icons/fa';
-import { RiLayoutGridFill } from 'react-icons/ri';
 import { OrderbookList } from 'src/components/market/orderbook-list';
 import { OrderbookProvider } from 'src/components/market/OrderbookContext';
 
 const MarketPage = () => {
   const { orderDrawerOpen, setOrderDrawerOpen, isOrderStateEmpty } = useOrderContext();
-  const { options, onChange, selected } = useToggleTab(['Orderbook', 'Assets'], 'Orderbook');
+  const { options, onChange, selected } = useToggleTab(['Orderbook'], 'Orderbook');
 
   const contents = (
     <>
@@ -16,24 +15,6 @@ const MarketPage = () => {
         <ToggleTab options={options} selected={selected} onChange={onChange} />
         <Spacer />
       </div>
-
-      {selected === 'Assets' && (
-        <>
-          <div className="flex space-x-2 items-center mb-2">
-            <Button variant="outline" className="font-heading">
-              Filter
-            </Button>
-            <Dropdown
-              label="Sort"
-              items={[
-                { label: 'High to low', onClick: console.log },
-                { label: 'Low to high', onClick: console.log }
-              ]}
-            />
-            <RiLayoutGridFill />
-          </div>
-        </>
-      )}
 
       {selected === 'Orderbook' && (
         <OrderbookProvider>
