cc532167f2a3a63d1aaca00c92ff47c27676569c
Steve Gehrman <sgehrman@gmail.com>
2022-05-01 21:47:49 -0700

    fix

--- src/components/feed/feed-filter-dropdown.tsx
+++ src/components/feed/feed-filter-dropdown.tsx
@@ -1,8 +1,7 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-import { Popover, Transition } from '@headlessui/react';
 import { FeedEventType } from '@infinityxyz/lib/types/core/feed';
-import { Checkbox } from '../common';
+import { Checkbox, PopoverButton } from '../common';
 
 interface FeedFilterDropdownProps {
   selectedTypes: Array<FeedEventType>;
@@ -29,49 +28,18 @@ export const FeedFilterDropdown: React.FC<FeedFilterDropdownProps> = ({ selected
     }
   ];
   return (
-    <Popover className="relative">
-      {() => (
-        <>
-          <Popover.Button
-            className={`
-            transition ease-in-out duration-300 hover:bg-gray-700  active:bg-gray-900
-            focus:outline-none focus-visible:ring focus:ring-black focus:ring-opacity-50
-             px-6 py-2
-             border rounded-3xl border-gray-300 text-gray-900
-             hover:text-white font-heading
-             false flex items-center space-x-1`}
-          >
-            <span>Filter</span>
-          </Popover.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel className="absolute z-10  max-w-sm px-4 mt-3 right-0 transform sm:px-0 ">
-              <div className="overflow-hidden rounded-lg  shadow-lg ring-1 ring-black ring-opacity-5">
-                <div className="p-5 bg-theme-light-50 w-80 space-y-4">
-                  {options.map((item, idx) => (
-                    <Checkbox
-                      boxOnLeft={false}
-                      key={idx}
-                      label={item.label}
-                      checked={selectedTypes.indexOf(item.value as FeedEventType) >= 0}
-                      onChange={(checked) => {
-                        onChange(checked, item.value);
-                      }}
-                    />
-                  ))}
-                </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
-    </Popover>
+    <PopoverButton title="Filter">
+      {options.map((item, idx) => (
+        <Checkbox
+          boxOnLeft={false}
+          key={idx}
+          label={item.label}
+          checked={selectedTypes.indexOf(item.value as FeedEventType) >= 0}
+          onChange={(checked) => {
+            onChange(checked, item.value);
+          }}
+        />
+      ))}
+    </PopoverButton>
   );
 };
