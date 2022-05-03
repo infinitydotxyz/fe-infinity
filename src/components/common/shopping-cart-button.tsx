10e2f1b0ad35c847a3c27ce0cae01d7c19607c0e
Thanh Tran <thanhprofession@gmail.com>
2022-05-03 13:17:52 -0700

    remove unused references

--- src/components/common/shopping-cart-button.tsx
+++ src/components/common/shopping-cart-button.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { useAppContext } from 'src/utils/context/AppContext';
-import { BiCopyAlt, BiCheck } from 'react-icons/bi';
-import { Dropdown } from './dropdown';
-import { useRouter } from 'next/router';
+
 import { Button } from './button';
 import { FaShoppingBag } from 'react-icons/fa';
 import { useOrderContext } from 'src/utils/context/OrderContext';
-
-interface ShoppingCartButtonProps {
-  address: string;
-  href: string;
-  label: string;
-  tooltip: string;
-  target?: string;
-}
 
 export const ShoppingCartButton: React.FC = () => {
   const { orderDrawerOpen, setOrderDrawerOpen, isOrderStateEmpty } = useOrderContext();
