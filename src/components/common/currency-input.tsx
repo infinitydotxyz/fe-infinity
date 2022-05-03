5523ed8150d823d482744b8f48048d60ebee4e46
Steve Gehrman <sgehrman@gmail.com>
2022-05-03 01:24:18 -0700

    fix

--- src/components/common/currency-input.tsx
+++ src/components/common/currency-input.tsx
@@ -30,28 +30,31 @@ export const CurrencyInput = ({ value, label, placeholder, onChange, autoFocus =
           />
         </div>
       </div>
-      <div className="dropdown z-10 relative flex items-center py-3 pl-5 pr-6 rounded-full bg-theme-light-300">
+      <div className="nft-dropdown z-10 relative flex items-center py-3 pl-5 pr-6 rounded-full bg-theme-light-300">
         <div className="uppercase text-heading mr-2">{currency}</div>
         <BiCaretDown className="h-3 w-3" />
 
-        <div className="  flex-col dropdown-menu top-12 right-0 absolute hidden text-gray-700 pt-1">
-          <div onClick={() => setCurrency('WETH')}>
-            <a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">
-              WETH
-            </a>
-          </div>
-          <div onClick={() => setCurrency('TEST')}>
-            <a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">
-              TEST
-            </a>
-          </div>
-          <div onClick={() => setCurrency('USD')}>
-            <a className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">
-              USD
-            </a>
-          </div>
+        <div className="flex-col nft-dropdown-menu top-12 right-0 absolute hidden text-gray-700 pt-1">
+          <MenuItem key="WETH" currency="WETH" onClick={(value) => setCurrency(value)} />
+          <MenuItem key="TEST" currency="TEST" onClick={(value) => setCurrency(value)} />
+          <MenuItem key="USD" currency="USD" onClick={(value) => setCurrency(value)} />
         </div>
       </div>
     </InputBox>
+  );
+};
+
+// ===================================================
+
+interface Props2 {
+  currency: string;
+  onClick: (value: string) => void;
+}
+
+const MenuItem = ({ currency, onClick }: Props2) => {
+  return (
+    <div onClick={() => onClick(currency)} className="bg-gray-200 hover:bg-gray-300 py-2 px-4 whitespace-no-wrap">
+      {currency}
+    </div>
   );
 };
