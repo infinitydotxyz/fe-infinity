d15b7f13304b5dc56d1882f04f6b832afbca8db9
Steve Gehrman <sgehrman@gmail.com>
2022-05-02 20:00:47 -0700

    terms page

---
+++ pages/terms-of-service/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageBox } from 'src/components/common';
+
+const TermsPage = () => {
+  return (
+    <PageBox title="Terms of Service">
+      <div className="flex flex-col items-center justify-center">Coming soon...</div>
+    </PageBox>
+  );
+};
+
+export default TermsPage;
