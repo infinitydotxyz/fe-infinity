c83ed905b200f3215db36795bbbdc103c02e6591
Thanh Tran <thanhprofession@gmail.com>
2022-05-05 13:26:42 -0700

    Adding <OrderDrawer /> component to the PageBox so that it's accessible on every page

--- src/components/common/page-box.tsx
+++ src/components/common/page-box.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 import { Navbar, Spacer, Header } from 'src/components/common';
+import { useOrderContext } from 'src/utils/context/OrderContext';
+import { OrderDrawer } from '../market';
 
 // used in the Header
 export const pageStyles = 'mx-auto desktop:w-5/6 desktop-sm:w-[95%] tabloid:w-[95%] mobile:w-[98%]';
@@ -22,6 +24,8 @@ export const PageBox = ({
   className = '',
   rightToolbar
 }: Props): JSX.Element => {
+  const { orderDrawerOpen, setOrderDrawerOpen } = useOrderContext();
+
   return (
     <>
       <div className="transition w-[100vw] h-[100vh] overflow-y-auto overflow-x-hidden justify-items-center">
@@ -31,7 +35,10 @@ export const PageBox = ({
           <div className={`transition ${fullWidth ? 'w-full' : pageStyles}`}>
             {showTitle ? <PageHeader title={title} rightToolbar={rightToolbar} /> : null}
 
-            <div className={`w-full ${className}`}>{children}</div>
+            <div className={`w-full ${className}`}>
+              {children}
+              <OrderDrawer open={orderDrawerOpen} onClose={() => setOrderDrawerOpen(false)} />
+            </div>
 
             {/* allows scroll so items aren't at the bottom of the screen */}
             <div style={{ height: 300 }} />
