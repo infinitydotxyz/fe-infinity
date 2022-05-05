2fc7e931b1982709cddc6cf5c1f978b3f5f88188
Thanh Tran <thanhprofession@gmail.com>
2022-05-05 09:53:18 -0700

    Updating Order Drawer to have Buy and Sell buttons when empty; shopping cart goes to Connect wallet if you click on it while not connected

--- src/components/common/drawer.tsx
+++ src/components/common/drawer.tsx
@@ -49,27 +49,29 @@ export const Drawer = ({ open, tooltip, subtitle, divide, onClose, title, childr
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="z-50 fixed inset-0 overflow-hidden" onClose={onClose}>
-        <div className="absolute inset-0 overflow-hidden">
-          <Dialog.Overlay className="absolute inset-0 backdrop-blur-xl" />
-          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
-            <Transition.Child
-              as={Fragment}
-              enter="transform transition ease-in-out duration-300 sm:duration-500"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leave="transform transition ease-in-out duration-300 sm:duration-500"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
-              <div className="pointer-events-auto w-screen max-w-md">
-                <div className="flex h-full flex-col overflow-y-auto bg-white shadow-xl">
-                  {header}
-                  <div className="flex h-full flex-col">{children}</div>
+        {open && (
+          <div className="absolute inset-0 overflow-hidden">
+            <Dialog.Overlay className="absolute inset-0 backdrop-blur-xl" />
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-300 sm:duration-500"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-300 sm:duration-500"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <div className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex h-full flex-col overflow-y-auto bg-white shadow-xl">
+                    {header}
+                    <div className="flex h-full flex-col">{children}</div>
+                  </div>
                 </div>
-              </div>
-            </Transition.Child>
+              </Transition.Child>
+            </div>
           </div>
-        </div>
+        )}
       </Dialog>
     </Transition.Root>
   );
