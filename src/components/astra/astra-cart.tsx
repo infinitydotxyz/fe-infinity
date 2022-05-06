180fde3bfcfab1459b80e7d099f4f723239b1432
Steve Gehrman <sgehrman@gmail.com>
2022-05-06 02:19:55 -0700

    fix

--- src/components/astra/astra-cart.tsx
+++ src/components/astra/astra-cart.tsx
@@ -15,30 +15,42 @@ export const AstraCart = ({ tokens, onRemove, onCheckout }: Props) => {
     <div className="w-48 h-full grid grid-rows-[1fr_auto]  grid-cols-[1fr]">
       <div className="row-span-1 col-span-1 flex flex-col space-y-2 items-start flex-1">
         {tokens.map((token, i) => {
-          return (
-            <div key={token.id} className="flex items-center w-full">
-              <div className="w-4 mr-2 text-right">{i + 1}.</div>
-              <BGImage className={twMerge(largeIconButtonStyle, 'rounded-lg')} url={token.image} />
-              <div className="ml-2">{token.tokenId}</div>
-
-              <Spacer />
-              <Button
-                size="plain"
-                variant="round"
-                onClick={() => {
-                  onRemove(token);
-                }}
-              >
-                <XIcon className={iconButtonStyle} />
-              </Button>
-            </div>
-          );
+          return <AstraCartItem key={token.id} token={token} index={i} onRemove={onRemove} />;
         })}
       </div>
 
       <div className="row-span-1 col-span-2 flex flex-col">
         <Button onClick={onCheckout}>Checkout</Button>
       </div>
+    </div>
+  );
+};
+
+// ====================================================================
+
+interface Props2 {
+  token: CardData;
+  index: number;
+  onRemove: (token: CardData) => void;
+}
+
+export const AstraCartItem = ({ token, index, onRemove }: Props2) => {
+  return (
+    <div key={token.id} className="flex items-center w-full">
+      <div className="w-4 mr-2 text-right">{index + 1}.</div>
+      <BGImage className={twMerge(largeIconButtonStyle, 'rounded-lg')} url={token.image} />
+      <div className="ml-2">{token.tokenId}</div>
+
+      <Spacer />
+      <Button
+        size="plain"
+        variant="round"
+        onClick={() => {
+          onRemove(token);
+        }}
+      >
+        <XIcon className={iconButtonStyle} />
+      </Button>
     </div>
   );
 };
--- src/components/astra/astra-navbar.tsx
+++ src/components/astra/astra-navbar.tsx
@@ -7,7 +7,7 @@ import { twMerge } from 'tailwind-merge';
 export const AstraNavbar = () => {
   return (
     // relative added to give it a different layer so shadow isn't wiped out by sidebar
-    <div className={twMerge('flex px-4 py-3 items-center bg-slate-100 border-b shadow-lg relative', inputBorderColor)}>
+    <div className={twMerge('flex px-4 py-3 items-center bg-slate-100 border-b shadow-md relative', inputBorderColor)}>
       <NextLink href="/" className="flex items-center">
         <SVG.miniLogo className={largeIconButtonStyle} />
         <div className="ml-4 text-2xl font-bold">Astra</div>
