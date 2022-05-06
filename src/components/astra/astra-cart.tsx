410282d0b03c8fdcb66f86ee4fb45b9640295bca
Steve Gehrman <sgehrman@gmail.com>
2022-05-06 04:51:58 -0700

    fix

--- src/components/astra/astra-cart.tsx
+++ src/components/astra/astra-cart.tsx
@@ -3,6 +3,7 @@ import { BGImage, Button, Spacer } from 'src/components/common';
 import { iconButtonStyle, largeIconButtonStyle } from 'src/utils/ui-constants';
 import { twMerge } from 'tailwind-merge';
 import { XIcon } from '@heroicons/react/outline';
+import { ReactNode } from 'react';
 
 interface Props {
   tokens: CardData[];
@@ -11,13 +12,35 @@ interface Props {
 }
 
 export const AstraCart = ({ tokens, onRemove, onCheckout }: Props) => {
-  return (
-    <div className="w-48 h-full grid grid-rows-[1fr_auto]  grid-cols-[1fr]">
-      <div className="row-span-1 col-span-1 flex flex-col space-y-2 items-start flex-1">
-        {tokens.map((token, i) => {
-          return <AstraCartItem key={token.id} token={token} index={i} onRemove={onRemove} />;
-        })}
+  const map = new Map<string, CardData[]>();
+
+  for (const token of tokens) {
+    const tkns = map.get(token.tokenAddress ?? '') ?? [];
+    tkns.push(token);
+    map.set(token.tokenAddress ?? '', tkns);
+  }
+
+  const divList: ReactNode[] = [];
+  let index = 0;
+  map.forEach((tokenArray) => {
+    const first = tokenArray[0];
+
+    divList.push(
+      <div className="w-full rounded-md overflow-hidden bg-gray-100 p-2 font-bold truncate" key={`header-${first.id}`}>
+        {first.collectionName}
       </div>
+    );
+
+    for (const t of tokenArray) {
+      divList.push(<AstraCartItem key={t.id} token={t} index={index++} onRemove={onRemove} />);
+    }
+
+    divList.push(<div key={Math.random()} className="h-1" />);
+  });
+
+  return (
+    <div className="w-48 h-full grid grid-rows-[1fr_auto] grid-cols-[1fr]">
+      <div className="w-48 row-span-1 col-span-1 flex flex-col space-y-2 items-start flex-1">{divList}</div>
 
       <div className="row-span-1 col-span-2 flex flex-col">
         <Button onClick={onCheckout}>Checkout</Button>
