c8b7c9eea7b60330d3bd9bb4ed6318a10bd12009
Steve Gehrman <sgehrman@gmail.com>
2022-05-07 02:30:07 -0700

    fix

--- src/components/common/bg-image.tsx
+++ src/components/common/bg-image.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface Props {
@@ -7,13 +8,40 @@ interface Props {
 }
 
 export const BGImage = ({ url, center = true, className = '' }: Props) => {
+  const [loaded, setLoaded] = useState(false);
+
   if (!url) {
     return <></>;
   }
 
+  useEffect(() => {
+    const img = new Image();
+    let deleted = false;
+
+    img.onload = () => {
+      if (!deleted) {
+        setLoaded(true);
+      }
+    };
+    img.src = url;
+
+    return () => {
+      deleted = true;
+
+      // trying to cancel load? not sure if this works
+      img.src = '';
+      img.onload = null;
+    };
+  }, []);
+
   return (
     <div
-      className={twMerge(center ? 'bg-center' : 'bg-top', 'w-full h-full bg-cover bg-no-repeat', className)}
+      className={twMerge(
+        center ? 'bg-center' : 'bg-top',
+        loaded ? 'opacity-100' : 'opacity-0',
+        'transition-opacity duration-500 w-full h-full bg-cover bg-no-repeat',
+        className
+      )}
       style={{ backgroundImage: `url(${url})` }}
     />
   );
