35d8ed80770ee5b19eae667972c95450d2eaa303
dylan <nducus@yahoo.com>
2022-05-20 16:29:47 -0700

    tab widths

--- src/components/common/toggle-tab.tsx
+++ src/components/common/toggle-tab.tsx
@@ -7,7 +7,7 @@ interface Props {
   className?: string;
   options: string[];
   onChange: (option: string) => void;
-  equalWidths?: boolean;
+  tabWidth?: string;
   altStyle?: boolean;
 }
 
@@ -34,14 +34,7 @@ export const useToggleTab = (options: string[], selectedOption: string) => {
   return { onChange, selected, options };
 };
 
-export const ToggleTab = ({
-  options,
-  onChange,
-  selected,
-  className = '',
-  equalWidths = true,
-  altStyle = false
-}: Props) => {
+export const ToggleTab = ({ options, onChange, selected, className = '', tabWidth = '', altStyle = false }: Props) => {
   return (
     <div className={`flex ${className}`}>
       <div
@@ -55,11 +48,12 @@ export const ToggleTab = ({
             <div
               key={option}
               className={twMerge(
-                equalWidths ? 'min-w-[130px]' : '',
+                tabWidth ? '' : 'min-w-[130px]',
                 'flex text-md items-center justify-center  text-center whitespace-nowrap font-medium rounded-full text-secondary',
                 selected === option ? 'bg-black text-white font-bold' : '',
                 altStyle ? 'py-1 px-6' : 'py-2.5 px-10'
               )}
+              style={{ minWidth: tabWidth ?? 'auto' }}
               onClick={() => onChange(option)}
             >
               {option}
