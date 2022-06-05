ff805e4f9915bf4dffa7e5d51ba971c8380588b6
dylan <nducus@yahoo.com>
2022-06-04 20:33:25 -0700

    fix

--- src/components/asset/trait/trait-list.tsx
+++ src/components/asset/trait/trait-list.tsx
@@ -12,17 +12,15 @@ export const TraitList: FC<TraitListProps> = ({ traits, collectionTraits }) => {
     <div className="mt-10">
       <p className="mt-4 sm:mt-6 sm:mb-4 tracking-base text-black font-bold">Traits</p>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-4 mt-6">
-        {traits?.map((trait: Erc721Attribute) => (
-          <Trait
-            key={trait.trait_type}
-            trait={trait}
-            description={
-              trait.trait_type
-                ? `${Math.floor(collectionTraits[trait.trait_type]?.values[trait?.value].percent) || 1} % have this`
-                : 'None'
-            }
-          />
-        ))}
+        {traits?.map((trait: Erc721Attribute) => {
+          let description = 'None';
+          if (trait?.trait_type && collectionTraits) {
+            description = `${
+              Math.floor(collectionTraits[trait.trait_type]?.values[trait?.value].percent) || 1
+            } % have this`;
+          }
+          return <Trait key={trait.trait_type} trait={trait} description={description} />;
+        })}
       </div>
     </div>
   );
