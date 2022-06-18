2fa0e031a61d79abd9176d596b2d1f7bfa13feb9
dylan <nducus@yahoo.com>
2022-06-18 15:52:16 -0700

    cancel modal: use OrderbookItem

--- src/components/asset/modals/cancel-modal.tsx
+++ src/components/asset/modals/cancel-modal.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { Collection, SignedOBOrder, Token } from '@infinityxyz/lib-frontend/types/core';
 import { Checkbox, Modal } from 'src/components/common';
 import { apiGet } from 'src/utils';
+import { uniqBy } from 'lodash';
+import { OrderbookItem } from 'src/components/market/orderbook-list/orderbook-item';
 
 interface Props {
   isOpen: boolean;
@@ -24,7 +26,9 @@ export const CancelModal = ({ isOpen, onClose, collection, token }: Props) => {
       requiresAuth: true
     });
     if (!error) {
-      setListings(result.data);
+      let orders: SignedOBOrder[] = result.data as SignedOBOrder[];
+      orders = uniqBy(orders, 'nonce'); // dedup orders with the same nonce (group of listed NFTs)
+      setListings(uniqBy(orders, 'nonce'));
     }
   };
 
@@ -50,17 +54,19 @@ export const CancelModal = ({ isOpen, onClose, collection, token }: Props) => {
         {listings.map((listing: SignedOBOrder, idx) => {
           return (
             <Checkbox
+              key={`${listing.id}_${idx}`}
               className="w-full mb-4"
               boxOnLeft={false}
               checked={selectedListings.includes(listing.id)}
               labelClassName="w-full"
               label={
                 <div className="w-full flex justify-between pr-4">
-                  <div className="w-2/3">Listing #{idx + 1}</div>
+                  {/* <div className="w-2/3">Listing #{idx + 1}</div>
                   <div className="w-1/3">
                     {listing.nfts.length} NFT{listing.nfts.length > 1 ? 's' : ''}
                   </div>
-                  <div className="w-1/3">{listing.startPriceEth} ETH</div>
+                  <div className="w-1/3">{listing.startPriceEth} ETH</div> */}
+                  <OrderbookItem nameItem={true} key={`${listing.id} ${listing.chainId}`} order={listing} />
                 </div>
               }
               onChange={(checked) => {
