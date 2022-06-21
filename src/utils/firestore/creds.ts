2430734ba18b32336549d961ddd048cb45c2fc17
nneverlander <findadit@gmail.com>
2022-06-21 18:34:59 +0300

    constants

--- src/utils/firestore/creds.ts
+++ src/utils/firestore/creds.ts
@@ -1,9 +1,9 @@
-export const firestoreConfig = {
-  apiKey: 'AIzaSyAGKBT_tFV1F1tVePXpzbWZmJCdfPbj9t0',
-  authDomain: 'nftc-dev.firebaseapp.com',
-  projectId: 'nftc-dev',
-  storageBucket: 'nftc-dev.appspot.com',
-  messagingSenderId: '824700732056',
-  appId: '1:824700732056:web:b1471c37dbd8afd0cc46ed',
-  measurementId: 'G-MXQRY19CMS'
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAv0Ls9j_R933xsdNa9TqXJkokRIGeTVCU',
+  authDomain: 'nftc-infinity.firebaseapp.com',
+  projectId: 'nftc-infinity',
+  storageBucket: 'nftc-infinity.appspot.com',
+  messagingSenderId: '161340494244',
+  appId: '1:161340494244:web:c4ad503ff42bd5f470b131',
+  measurementId: 'G-EQYW08WPY0'
 };
