b166b5ae1d4190e5c9f839241724f5747d699ed6
dylan <nducus@yahoo.com>
2022-06-30 16:20:05 -0700

    lint

--- src/components/common/toggle-tab.tsx
+++ src/components/common/toggle-tab.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { historyPushState } from 'src/utils';
 
 interface Props {
   selected: string;
