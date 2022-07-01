0860ca39aaddc43a86945cf190a28ebdb277768c
dylan <nducus@yahoo.com>
2022-06-30 23:48:36 -0700

    format

--- src/utils/commonUtils.tsx
+++ src/utils/commonUtils.tsx
@@ -121,6 +121,27 @@ export const formatNumber = (floatNum: number | undefined, decimals = 0): string
   }
 };
 
+// example: nFormatter(1234, 1) = > 1.2K
+export function nFormatter(num: number, digits = 2) {
+  const lookup = [
+    { value: 1, symbol: '' },
+    { value: 1e3, symbol: 'K' },
+    { value: 1e6, symbol: 'M' },
+    { value: 1e9, symbol: 'G' },
+    { value: 1e12, symbol: 'T' },
+    { value: 1e15, symbol: 'P' },
+    { value: 1e18, symbol: 'E' }
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return item ? (num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol : '0';
+}
+
 export const getCustomExceptionMsg = (msg: ReactNode) => {
   let customMsg = msg;
   if (typeof msg === 'string' && msg.indexOf('err: insufficient funds for gas * price + value') > 0) {
