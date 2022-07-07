75a6b3343558920c98a10e0de668f6a7a93ae8b8
dylan <nducus@yahoo.com>
2022-07-06 23:42:21 -0700

    fix

--- src/components/common/eth-price.tsx
+++ src/components/common/eth-price.tsx
@@ -1,16 +1,25 @@
+import { twMerge } from 'tailwind-merge';
+
 interface Props {
   label: string;
   className?: string;
+  rowClassName?: string;
   ethClassName?: string;
   labelClassName?: string;
 }
 
 export const EthSymbol = 'Ξ';
 
-export const EthPrice = ({ label, className = '', ethClassName = '', labelClassName = '' }: Props): JSX.Element => {
+export const EthPrice = ({
+  label,
+  className = '',
+  rowClassName = '',
+  ethClassName = '',
+  labelClassName = ''
+}: Props): JSX.Element => {
   return (
     <div className={className}>
-      <div className="flex items-center">
+      <div className={twMerge(`flex items-center ${rowClassName}`)}>
         <div className={`pr-2 font-bold ${ethClassName}`}>{EthSymbol}</div>
 
         <div className={labelClassName}>{label}</div>
