9bec22ca8b57f954cf64018f52ebd7c296a100df
Sleeyax <yourd3veloper@gmail.com>
2022-07-07 23:30:36 +0200

    use goerli token contract address from lib

--- src/hooks/contract/token/useTokenContract.ts
+++ src/hooks/contract/token/useTokenContract.ts
@@ -1,8 +1,9 @@
 import { ERC20ABI } from '@infinityxyz/lib-frontend/abi/erc20';
+import { GOERLI_TOKEN_CONTRACT_ADDRESS } from '@infinityxyz/lib-frontend/utils/constants';
 import { useContract } from '../useContract';
 
 export function useTokenContract() {
-  const address = '0x2BDB98086d47e38e3A40B42463Af005F5CF72146'; // TODO: prod address
+  const address = GOERLI_TOKEN_CONTRACT_ADDRESS;
   const contract = useContract(address, ERC20ABI);
 
   return { address, contract };
