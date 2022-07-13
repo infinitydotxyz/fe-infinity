09a64ff582f023a1be452fa69a67ad8e899d86f9
nneverlander <findadit@gmail.com>
2022-07-13 22:33:57 +0200

    removes fs subscribe code

--- src/utils/firestore/counterUtils.ts
+++ src/utils/firestore/counterUtils.ts
@@ -1,16 +1,17 @@
+import { firestoreConstants } from '@infinityxyz/lib-frontend/utils';
 import { doc } from 'firebase/firestore';
 import { FirestoreDistributedCounter } from './FirestoreDistributedCounter';
-import { COLL_FEED, firestoreDb } from './firestoreUtils';
+import { firestoreDb } from './firestoreUtils';
 
 export async function increaseLikes(userAccount: string, itemId: string) {
-  const docRef = doc(firestoreDb, `${COLL_FEED}/${itemId}`);
+  const docRef = doc(firestoreDb, `${firestoreConstants.FEED_COLL}/${itemId}`);
   // eslint-disable-next-line
   const likes = new FirestoreDistributedCounter(docRef as any, 'likes');
   likes.incrementBy(1);
 }
 
 export async function increaseComments(userAccount: string, itemId: string) {
-  const docRef = doc(firestoreDb, `${COLL_FEED}/${itemId}`);
+  const docRef = doc(firestoreDb, `${firestoreConstants.FEED_COLL}/${itemId}`);
   // eslint-disable-next-line
   const comments = new FirestoreDistributedCounter(docRef as any, 'comments');
   comments.incrementBy(1);
