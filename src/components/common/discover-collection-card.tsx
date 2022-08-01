e5b42e5a399adbf99d0d939ccdca24833ff0dab3
Steve Gehrman <sgehrman@gmail.com>
2022-08-01 16:52:45 -0700

    fix

--- src/components/common/discover-collection-card.tsx
+++ src/components/common/discover-collection-card.tsx
@@ -2,9 +2,9 @@ import { formatNumber, getOptimizedCloudImage } from 'src/utils';
 import { NextLink } from './next-link';
 import { SVG } from './svg';
 import { EthPrice } from './eth-price';
-import { DiscoverOrderBy } from 'pages/orderbook';
 import { CollectionStatsDto } from '@infinityxyz/lib-frontend/types/dto/stats';
 import { EZImage } from './ez-image';
+import { DiscoverOrderBy } from './discover-collection-grid';
 
 interface Props {
   orderBy: DiscoverOrderBy;
