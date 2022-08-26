7f2aab5def31ccedf984a0bae72aef895c9369f8
Sleeyax <yourd3veloper@gmail.com>
2022-08-26 18:29:18 +0200

    Further divide info into sections

--- pages/rewards/global-rewards.tsx
+++ pages/rewards/global-rewards.tsx
@@ -13,7 +13,7 @@ const GlobalRewards: React.FC = () => {
               During this epoch, the exchange product is launched in beta alongside the $NFT token and the Infinity NFT
               collection. This epoch has 4 token rewards phases.
             </p>
-            <Heading as="h3" className="text-3xl !font-body !font-medium">
+            <Heading as="h3" className="text-3xl !font-body !font-medium !text-black">
               Benefits
             </Heading>
             <p className="my-2">Holders of the Infinity NFT are MVPs with benefits including (but not limited to):</p>
@@ -49,24 +49,57 @@ const GlobalRewards: React.FC = () => {
           </InfoBox.Stats>
           <InfoBox.Stats
             title="Phase 2"
-            description=" 3% of the total supply (60M tokens) are given out as trading rewards in the form of fee refunds. Every 1$ in fees paid gets 33 $NFT tokens. Buyers with greater than 5 ETH volume will also get a free mint of the Infinity NFT.
-            "
+            description={
+              <>
+                <p>
+                  <strong>3%</strong> of the total supply (<strong>60M tokens</strong>) are given out as trading rewards
+                  in the form of fee refunds. Every <strong>1$</strong> in fees paid gets <strong>33 $NFT</strong>{' '}
+                  tokens. Buyers with greater than <strong>5 ETH volume</strong> will also get a{' '}
+                  <strong>free mint</strong> of the Infinity NFT.
+                </p>
+              </>
+            }
           >
             <InfoBox.Stat label="Progress" value="0%" />
+            <InfoBox.Stat label="Trading Rewards" value="3%" />
+            <InfoBox.Stat label="$ / $NFT ratio" value="1 / 33" />
+            <InfoBox.Stat label="Free Infinity NFT Mint" value="+5 ETH" />
           </InfoBox.Stats>
           <InfoBox.Stats
             title="Phase 3"
-            description="4% of the total supply (80M tokens) are given out as trading rewards in the form of fee refunds. Every 1$ in fees paid gets 25 $NFT tokens. Buyers with greater than 10 ETH volume will also get a free mint of the Infinity NFT.
-            "
+            description={
+              <>
+                <p>
+                  <strong>4%</strong> of the total supply (<strong>80M tokens</strong>) are given out as trading rewards
+                  in the form of fee refunds. Every <strong>1$</strong> in fees paid gets <strong>25 $NFT</strong>{' '}
+                  tokens. Buyers with greater than <strong>10 ETH volume</strong> will also get a{' '}
+                  <strong>free mint</strong> of the Infinity NFT.
+                </p>
+              </>
+            }
           >
             <InfoBox.Stat label="Progress" value="0%" />
+            <InfoBox.Stat label="Trading Rewards" value="4%" />
+            <InfoBox.Stat label="$ / $NFT ratio" value="1 / 25" />
+            <InfoBox.Stat label="Free Infinity NFT Mint" value="+10 ETH" />
           </InfoBox.Stats>
           <InfoBox.Stats
             title="Phase 4"
-            description=" 5% of the total supply (100M tokens) are given out as trading rewards in the form of fee refunds. Every 1$ in fees paid gets 20 $NFT tokens. Buyers with greater than 20 ETH volume will also get a free mint of the Infinity NFT.
-            "
+            description={
+              <>
+                <p>
+                  <strong>5%</strong> of the total supply (<strong>100M tokens</strong>) are given out as trading
+                  rewards in the form of fee refunds. Every <strong>1$</strong> in fees paid gets{' '}
+                  <strong>20 $NFT</strong> tokens. Buyers with greater than <strong>20 ETH volume</strong> will also get
+                  a <strong>free mint</strong> of the Infinity NFT.
+                </p>
+              </>
+            }
           >
             <InfoBox.Stat label="Progress" value="0%" />
+            <InfoBox.Stat label="Trading Rewards" value="5%" />
+            <InfoBox.Stat label="$ / $NFT ratio" value="1 / 20" />
+            <InfoBox.Stat label="Free Infinity NFT Mint" value="+20 ETH" />
           </InfoBox.Stats>
         </InfoBox.SideInfo>
       </InfoBox>
@@ -80,7 +113,10 @@ const GlobalRewards: React.FC = () => {
               rewards in ETH (see token utility section). The free Infinity NFT mint is no longer available starting
               this epoch.
             </p>
-            <ul className="list-disc">
+            <Heading as="h3" className="text-3xl !font-body !font-medium !text-black">
+              Benefits
+            </Heading>
+            <ul className="list-disc ml-4">
               <li>Curators earn ETH rewards. </li>
             </ul>
           </>
@@ -89,10 +125,19 @@ const GlobalRewards: React.FC = () => {
         <InfoBox.SideInfo>
           <InfoBox.Stats
             title="Phase"
-            description="7% of the total supply (140M tokens) are given out as trading rewards in the form of fee refunds. Every
-                1$ in fees paid gets 10 $NFT tokens."
+            description={
+              <>
+                <p>
+                  <strong>7%</strong> of the total supply (<strong>140M tokens</strong>) are given out as trading
+                  rewards in the form of fee refunds. Every <strong>1$</strong> in fees paid gets{' '}
+                  <strong>10 $NFT</strong> tokens.
+                </p>
+              </>
+            }
           >
             <InfoBox.Stat label="Progress" value="0%" />
+            <InfoBox.Stat label="Trading Rewards" value="7%" />
+            <InfoBox.Stat label="$ / $NFT ratio" value="1 / 10" />
           </InfoBox.Stats>
         </InfoBox.SideInfo>
       </InfoBox>
@@ -102,7 +147,10 @@ const GlobalRewards: React.FC = () => {
         description={
           <>
             <p className="my-2">Starting this epoch, token emissions go to zero. Curators still earn ETH rewards.</p>
-            <ul className="list-disc my-2">
+            <Heading as="h3" className="text-3xl !font-body !font-medium !text-black">
+              Benefits
+            </Heading>
+            <ul className="list-disc my-2 ml-4">
               <li>All ETH rewards go to curators.</li>
               <li>
                 Only the tokens in circulation are used for staking that gives users voting power. In other words, the
@@ -116,7 +164,7 @@ const GlobalRewards: React.FC = () => {
         }
       >
         <InfoBox.SideInfo>
-          <InfoBox.Stats title="Phase">
+          <InfoBox.Stats title="Phase" description="Token emissions go to zero. Curators still earn ETH rewards.">
             <InfoBox.Stat label="Progress" value="0%" />
           </InfoBox.Stats>
         </InfoBox.SideInfo>
