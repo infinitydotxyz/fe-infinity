c294227ddda1d28163beac8e091e121e3bfab751
nneverlander <findadit@gmail.com>
2022-09-13 16:21:22 +0200

    minor

--- pages/curation/index.tsx
+++ pages/curation/index.tsx
@@ -58,10 +58,10 @@ export default function Curation() {
         <strong>{Object.keys(votes).length}</strong> <span>collections?</span>
       </Modal>
       <div className="flex justify-between mb-8">
-        <div>
+        <div className="mt-3">
           <span className="p-4 border border-gray-300 rounded-3xl mr-2">
             <strong className="mr-2">{votesQuota}</strong>
-            <span># votes available</span>
+            <span>votes available</span>
           </span>
           {votesQuota > 0 && <Button onClick={showModal}>Confirm</Button>}
         </div>
