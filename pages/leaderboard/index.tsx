189c488a3e028929755292585f6d061f661be668
Sleeyax <yourd3veloper@gmail.com>
2022-09-16 00:14:08 +0200

    fetch leaderboard data from BE

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,61 +1,61 @@
-import { EthPrice, NextLink, PageBox } from 'src/components/common';
+import { Phase } from '@infinityxyz/lib-frontend/types/core';
+import { EZImage, NextLink, PageBox, Spinner } from 'src/components/common';
+import { useRaffleLeaderboard } from 'src/hooks/api/useRaffleLeaderboard';
 import { formatNumber, nFormatter } from 'src/utils';
 
-const LeaderboardPage = () => {
-  // const { isDesktop } = useScreenSize();
+const CURREN_PHASE = Phase.One; // TODO: dynamically get current phase from somewhere?
 
-  // TODO: fetch real data from BE
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const data: any[] = [
-    { address: '0x0000000001233', numBuys: 123, numSales: 12, volume: 10255, stakeLevel: 'idk', tickets: 10 },
-    { address: '0x0000000001233', numBuys: 123, numSales: 12, volume: 10255, stakeLevel: 'idk', tickets: 10 },
-    { address: '0x0000000001233', numBuys: 123, numSales: 12, volume: 10255, stakeLevel: 'idk', tickets: 10 }
-  ];
+const LeaderboardPage = () => {
+  const { result: raffles, isLoading } = useRaffleLeaderboard(CURREN_PHASE);
 
   return (
-    <PageBox title="Leaderboard">
+    <PageBox title={`Leaderboard (${CURREN_PHASE})`}>
       <div className="space-y-4 mt-8">
-        {data.map((entry) => {
-          return (
-            <div
-              key={entry.address}
-              className="bg-theme-light-200 px-10 h-[110px] rounded-3xl flex items-center font-heading"
-            >
-              <div className="flex justify-between items-center w-full ml-6">
-                <NextLink href={`/user/${entry.address}`} className="truncate">
-                  {entry.address}
-                </NextLink>
+        {isLoading && <Spinner />}
+        {!isLoading &&
+          raffles.map((raffle) => {
+            return (
+              <div
+                key={raffle.userAddress}
+                className="bg-theme-light-200 px-10 h-[110px] rounded-3xl flex items-center font-heading"
+              >
+                <div className="flex justify-between items-center w-full ml-6">
+                  <NextLink href={`/user/${raffle.userAddress}`}>
+                    <EZImage className="w-16 h-16 rounded-2xl overflow-clip" src={raffle.user.profileImage} />
+                  </NextLink>
 
-                <div className="w-1/9 max-w-[80px] min-w-[80px]">
-                  <div className="text-black font-bold font-body flex items-center">Sales</div>
-                  <div>{formatNumber(entry.numSales)}</div>
-                </div>
+                  <NextLink href={`/user/${raffle.userAddress}`} className="truncate">
+                    {raffle.user.displayName || raffle.user.address || raffle.userAddress}
+                  </NextLink>
 
-                <div className="w-1/9 max-w-[80px] min-w-[80px]">
-                  <div className="text-black font-bold font-body flex items-center">Volume</div>
-                  <div>
-                    <EthPrice label={`${entry.volume ? nFormatter(entry.volume) : '-'}`} />
+                  <div className="w-1/9 max-w-[80px] min-w-[80px]">
+                    <div className="text-black font-bold font-body flex items-center">Tickets</div>
+                    <div>{formatNumber(raffle.numTickets)}</div>
+                  </div>
+
+                  <div className="w-1/9 max-w-[80px] min-w-[80px]">
+                    <div className="text-black font-bold font-body flex items-center">Volume</div>
+                    <div>{`${raffle.volumeUSDC ? nFormatter(raffle.volumeUSDC) : '-'}`} USDC</div>
+                  </div>
+
+                  <div className="w-1/9 max-w-[80px] min-w-[80px]">
+                    <div className="text-black font-bold font-body flex items-center">Rank</div>
+                    <div>{raffle.rank}</div>
+                  </div>
+
+                  <div className="w-1/9 max-w-[80px] min-w-[80px]">
+                    <div className="text-black font-bold font-body flex items-center">Luckiness</div>
+                    <div>{raffle.chanceOfWinning}%</div>
+                  </div>
+
+                  <div className="w-1/9 max-w-[80px] min-w-[80px]">
+                    <div className="text-black font-bold font-body flex items-center">Tickets</div>
+                    <div>{formatNumber(raffle.numTickets)}</div>
                   </div>
                 </div>
-
-                <div className="w-1/9 max-w-[80px] min-w-[80px]">
-                  <div className="text-black font-bold font-body flex items-center">Buys</div>
-                  <div>{entry.numBuys ? formatNumber(entry.numBuys) : '-'}</div>
-                </div>
-
-                <div className="w-1/9 max-w-[80px] min-w-[80px]">
-                  <div className="text-black font-bold font-body flex items-center">Stake Level</div>
-                  <div>{entry.stakeLevel}</div>
-                </div>
-
-                <div className="w-1/9 max-w-[80px] min-w-[80px]">
-                  <div className="text-black font-bold font-body flex items-center">Tickets</div>
-                  <div>{formatNumber(entry.tickets)}</div>
-                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </PageBox>
   );
