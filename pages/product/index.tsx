e109d3836b70b16a2539e854345dca622c0e8b5b
Steve Gehrman <sgehrman@gmail.com>
2022-09-19 03:50:14 -0700

    fix

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -5,6 +5,7 @@ import { AudioWave } from 'src/components/product/audio';
 import { Burst1 } from 'src/components/product/burst1';
 import { Burst2 } from 'src/components/product/burst2';
 import { Burst3 } from 'src/components/product/burst3';
+import { InfinitySvg } from 'src/components/product/infinity';
 
 const ProductPage = () => {
   return (
@@ -12,13 +13,17 @@ const ProductPage = () => {
       <HomeNavBar />
 
       <div className="  relative ">
+        <InfinitySvg />
         <Clouds />
 
-        <div
-          className=" absolute top-14 right-0 left-0 bottom-0 text-center font-bold   text-white  "
-          style={{ fontSize: '14vw', textShadow: '5px 10px 33px #00227766' }}
-        >
-          Infinity
+        <div className=" absolute leading-none top-14 right-0 left-0  text-start flex flex-col items-center font-bold   text-white  ">
+          <div style={{ fontSize: '14vw', textShadow: '5px 10px 33px #00227766' }}>Infinity</div>
+          <div
+            className="  relative  -top-2    font-bold   text-white  "
+            style={{ fontSize: '3vw', textShadow: '5px 10px 33px #002277aa' }}
+          >
+            Exchange
+          </div>
         </div>
       </div>
 
