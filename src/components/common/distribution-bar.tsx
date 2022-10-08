07d85768467f02762a42c767481ed356e8b73acb
Steve Gehrman <sgehrman@gmail.com>
2022-10-07 20:57:56 -0700

    fix

--- src/components/common/distribution-bar.tsx
+++ src/components/common/distribution-bar.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from 'tailwind-merge';
+import { Spacer } from './spacer';
 
 interface Props {
   distribution: { percent: number; label: string; className?: string }[];
@@ -6,18 +7,19 @@ interface Props {
 
 export const DistributionBar = ({ distribution }: Props) => {
   return (
-    <div className="bg-gray-100 rounded-r-3xl w-full">
-      {distribution.map((item) => {
+    <div className="bg-gray-100 rounded-3xl px-5 py-3 w-full">
+      {distribution.map((item, index) => {
         return (
-          <div className="relative w-full">
+          <div key={index} className="relative w-full">
             <div
-              className={twMerge('text-sm font-normal rounded-r-3xl py-4', item.className)}
+              className={twMerge('text-sm font-normal rounded-r-3xl py-4 mt-1', item.className)}
               style={{ maxWidth: `${item.percent}%` }}
             ></div>
-            <div className="absolute top-1 z-5 w-full">
-              <div className="space-x-2 px-1 pr-4 font-heading w-full flex align-between justify-between">
-                <div className="w-[80px] m-0 font-bold">{`${item.percent}%`}</div>
-                <div className="w-[200px] m-0 text-right">{item.label}</div>
+            <div className="absolute top-1 w-full">
+              <div className="font-heading w-full flex items-center px-3">
+                <div className="font-bold">{`${item.percent}%`}</div>
+                <Spacer />
+                <div className="text-right text-sm">{item.label}</div>
               </div>
             </div>
           </div>
