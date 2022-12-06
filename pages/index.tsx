a406b335cb7d188a2a77078ff61b5a240b3c333f
Steve Gehrman <sgehrman@gmail.com>
2022-12-05 20:42:33 -0800

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { CuratedCollectionsOrderBy } from '@infinityxyz/lib-frontend/types/dto/c
 import { useRouter } from 'next/router';
 import { RaffleDescription } from 'src/components/raffles/raffle-description';
 import { useRaffles } from 'src/hooks/api/useRaffles';
-import { Button, CenteredContent, PageBox, Spacer, Spinner } from 'src/components/common';
+import { Button, CenteredContent, PageBox, pageStyles, Spacer, Spinner } from 'src/components/common';
 import { FavoritesDescription } from 'src/components/favorites/favorites-description';
 import { GlobalFeedList } from 'src/components/feed-list/global-feed-list';
 import { AllCuratedStart } from 'src/components/start/all-curated-start';
@@ -12,6 +12,7 @@ import { TrendingStart } from 'src/components/start/trending-start';
 import { useFavorites } from 'src/hooks/api/useFavorites';
 import { twMerge } from 'tailwind-merge';
 import GlobalRewards from './rewards/global-rewards';
+import { ReactNode } from 'react';
 
 const HomePage = () => {
   const router = useRouter();
@@ -29,27 +30,39 @@ const HomePage = () => {
   };
 
   return (
-    <PageBox title="Home" showTitle={false} footer={<StartFooter />}>
-      {titleHeader('Recently Curated Collections', 'mt-6', '/curated?tab=All+Curated')}
-      <AllCuratedStart orderBy={CuratedCollectionsOrderBy.Timestamp} />
+    <PageBox title="Home" fullWidth showTitle={false} footer={<StartFooter />}>
+      <HomeSection theme="white">
+        {titleHeader('Recently Curated Collections', 'mt-6', '/curated?tab=All+Curated')}
+        <AllCuratedStart orderBy={CuratedCollectionsOrderBy.Timestamp} />
+      </HomeSection>
 
-      {titleHeader('Rewards', 'mt-10', '/rewards?tab=Global+Rewards')}
-      <GlobalRewards showCount={1} />
+      <HomeSection theme="red">
+        {titleHeader('Rewards', 'mt-10', '/rewards?tab=Global+Rewards')}
+        <GlobalRewards showCount={1} />
+      </HomeSection>
 
-      {titleHeader('Favorites', 'mt-10', '/favorites')}
-      <FavoritesPanel />
+      <HomeSection theme="blue">
+        {titleHeader('Favorites', 'mt-10', '/favorites')}
+        <FavoritesPanel />
+      </HomeSection>
 
-      {titleHeader('Raffles', 'mt-10', '/raffles')}
-      <RafflesPanel />
+      <HomeSection theme="black">
+        {titleHeader('Raffles', 'mt-10', '/raffles')}
+        <RafflesPanel />
+      </HomeSection>
 
-      {titleHeader('Trending', 'mt-8', '/trending')}
-      <TrendingStart />
+      <HomeSection theme="red">
+        {titleHeader('Trending', 'mt-8', '/trending')}
+        <TrendingStart />
+      </HomeSection>
 
-      {titleHeader('Feed', 'mt-10', '/feed')}
-      <GlobalFeedList
-        types={[EventType.TwitterTweet, EventType.DiscordAnnouncement, EventType.CoinMarketCapNews]}
-        compact={true}
-      />
+      <HomeSection theme="blue">
+        {titleHeader('Feed', 'mt-10', '/feed')}
+        <GlobalFeedList
+          types={[EventType.TwitterTweet, EventType.DiscordAnnouncement, EventType.CoinMarketCapNews]}
+          compact={true}
+        />
+      </HomeSection>
     </PageBox>
   );
 };
@@ -113,6 +126,38 @@ const RafflesPanel = () => {
           return <RaffleDescription raffle={raffle} key={raffle.id} ethPrice={ethPrice} />;
         }
       })}
+    </div>
+  );
+};
+
+// ===========================================================
+
+interface Props {
+  children: ReactNode;
+  theme: 'red' | 'white' | 'blue' | 'black';
+}
+
+const HomeSection = ({ children, theme }: Props) => {
+  let bg = '';
+
+  switch (theme) {
+    case 'red':
+      bg = 'bg-red-500';
+      break;
+    case 'white':
+      break;
+
+    case 'blue':
+      bg = 'bg-blue-500';
+      break;
+
+    case 'black':
+      bg = 'bg-black';
+      break;
+  }
+  return (
+    <div className={twMerge(bg, 'pt-7 pb-24')}>
+      <div className={pageStyles}>{children}</div>
     </div>
   );
 };
--- todo.md
+++ todo.md
@@ -1,18 +1,12 @@
 // export const API_BASE = 'http://localhost:9090';
 export const API_BASE = 'https://sv.infinity.xyz';
 
-http://localhost:3000/collection/manekigang?tab=Reservoir
-[9:41 PM]
+http://localhost:3000/collection/manekigang?tab=NFTs
 
-1. after add to order is clicked, btn state doesn't change to added
-   [9:41 PM]
-2. no filters on the tab? can you add price filters?
-   [9:42 PM]
-3. what are these items? are they available to buy? if so, where is the price displayed? (edited)
-   [9:42 PM]
-   On the NFTs tab:
-   http://localhost:3000/collection/manekigang?tab=NFTs
-   [9:43 PM]
-   also add a 'add to cart' btn next to 'add to order' btn
-   [9:44 PM]
-   'add to cart' is simply a shortcut that adds single item orders to cart directly instead of 'add item to order' -> 'add order to cart' flow
+[9:43 PM]
+also add a 'add to cart' btn next to 'add to order' btn
+[9:44 PM]
+'add to cart' is simply a shortcut that adds single item orders to cart directly instead of 'add item to order' -> 'add order to cart' flow
+
+// home page
+https://www.figma.com/file/R14MZXLv336FbK7RNsEZOa/Homepage-reskin?node-id=0%3A1
