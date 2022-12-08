ba8ca6997000821bbbcf7d75dd87c817e7ca7972
Steve Gehrman <sgehrman@gmail.com>
2022-12-08 03:11:43 -0800

    fix

--- src/components/astra/astra-sidebar.tsx
+++ src/components/astra/astra-sidebar.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { CollectionList } from 'src/components/astra/collection-list';
-import { DebouncedTextInputBox } from 'src/components/common';
 import { inputBorderColor } from 'src/utils/ui-constants';
 import { twMerge } from 'tailwind-merge';
 // import { useLocation } from 'react-router';
 // import queryString from 'query-string';
 import { CollectionInfo, getCollection } from 'src/utils/astra-utils';
 import { useDashboardContext } from 'src/utils/context/DashboardContext';
+import debounce from 'lodash/debounce';
 
 interface Props {
   onClick: (value: CollectionInfo) => void;
@@ -60,8 +60,7 @@ export const AstraSidebar = ({ onClick, onLoad, selectedCollection }: Props) => 
   return (
     <div className="flex flex-col h-full">
       <div className={twMerge(inputBorderColor, 'px-4 py-4')}>
-        <DebouncedTextInputBox
-          label=""
+        <DbouncedTextField
           value={query}
           placeholder="Search"
           onChange={(value) => {
@@ -73,6 +72,43 @@ export const AstraSidebar = ({ onClick, onLoad, selectedCollection }: Props) => 
       <div className={twMerge(inputBorderColor, 'overflow-y-scroll h-full overflow-x-hidden w-full px-4')}>
         {collectionsList}
       </div>
+    </div>
+  );
+};
+
+// ================================================
+
+interface Props3 {
+  value: string;
+  onChange: (query: string) => void;
+  placeholder: string;
+  className?: string;
+}
+
+export const DbouncedTextField = ({ value, placeholder, onChange, className = '' }: Props3) => {
+  const [query, setQuery] = useState(value);
+
+  const handleChange = (value: string) => {
+    setQuery(value);
+    setQueryDebounced(value);
+  };
+
+  // must use useCallback or it doesn't work
+  const setQueryDebounced = useCallback(
+    debounce((value: string) => {
+      onChange(value);
+    }, 300),
+    []
+  );
+
+  return (
+    <div className={className}>
+      <input
+        value={query}
+        placeholder={placeholder}
+        className="w-full outline-none dark:bg-dark-card bg-gray-100 dark:text-dark-body text-light-body rounded-full focus-visible:ring focus:ring-0 py-2 px-4 text-lg lg:text-md leading-5"
+        onChange={(event) => handleChange(event.target.value)}
+      />
     </div>
   );
 };
