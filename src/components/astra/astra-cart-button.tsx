4c458d393719aecadc27967b3826e9fe604ef420
Steve Gehrman <sgehrman@gmail.com>
2022-12-12 21:21:45 -0800

    fix

--- src/components/astra/astra-cart-button.tsx
+++ src/components/astra/astra-cart-button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaShoppingCart } from 'react-icons/fa';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
 import { iconButtonStyle, primaryTextColor } from 'src/utils/ui-constants';
 import { ARoundButton } from './astra-button';
 import { useDashboardContext } from 'src/utils/context/DashboardContext';
@@ -10,7 +10,7 @@ export const AstraCartButton = () => {
 
   return (
     <ARoundButton onClick={() => setShowCart(!showCart)} className="relative">
-      <FaShoppingCart className={twMerge(iconButtonStyle, showCart ? primaryTextColor : '')} />
+      <HiOutlineShoppingCart className={twMerge(iconButtonStyle, showCart ? primaryTextColor : '')} />
       {selection.length > 0 ? (
         <span className="px-1.5 absolute top-1 right-1.5   bg-red-500   text-white rounded-full text-xs">
           {selection.length}
