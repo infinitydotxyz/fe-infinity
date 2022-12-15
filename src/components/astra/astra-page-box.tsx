1313668dcc9e31768f9bdad8aa5182b618128c58
Steve Gehrman <sgehrman@gmail.com>
2022-12-15 04:57:04 -0800

    fix

--- src/components/astra/astra-page-box.tsx
+++ src/components/astra/astra-page-box.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 export const APageBox = ({ children, title, showTitle = true, className = '', rightToolbar }: Props): JSX.Element => {
   return (
-    <div className={twMerge('transition  w-full h-full flex flex-col overflow-y-clip overflow-x-clip', className)}>
+    <div className={twMerge('transition pl-6 w-full h-full flex flex-col overflow-y-clip overflow-x-clip', className)}>
       <Header title={title} />
 
       {showTitle ? <APageHeader title={title} rightToolbar={rightToolbar} /> : null}
