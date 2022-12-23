13fac125c1b51535c8cee97d568637575e8ce4a7
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-12-23 12:38:03 -0600

    removed is prod check

--- pages/orderbook/index.tsx
+++ pages/orderbook/index.tsx
@@ -3,7 +3,6 @@ import { OrderbookContent } from 'src/components/orderbook/orderbook-list';
 import { OrderbookProvider } from 'src/components/orderbook/OrderbookContext';
 import { useRouter } from 'next/router';
 import { OrderbookGraph } from 'src/components/orderbook/graph/orderbook-graph';
-import { isProd } from 'src/utils';
 import { APageBox } from 'src/components/astra/astra-page-box';
 import { textClr } from 'src/utils/ui-constants';
 import { twMerge } from 'tailwind-merge';
@@ -11,7 +10,7 @@ import { twMerge } from 'tailwind-merge';
 const OrderbookPage = () => {
   const router = useRouter();
 
-  const tabs = isProd() ? ['List view', 'Graph view'] : ['List view', 'Graph view'];
+  const tabs = ['List view', 'Graph view'];
 
   const { options, onChange, selected } = useToggleTab(tabs, (router?.query?.tab as string) || 'List view');
 
