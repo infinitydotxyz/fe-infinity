a4e130d650e7f56f3aedfb330fb4569d517bbf31
Sleeyax <yourd3veloper@gmail.com>
2022-12-24 18:29:07 +0100

    redirect collection page to items tab

--- pages/v3/collection/[name]/index.tsx
+++ pages/v3/collection/[name]/index.tsx
@@ -1,12 +1,5 @@
-import { APageBox } from 'src/components/astra/astra-page-box';
-import { DashboardAll } from 'src/components/astra/dashboard/dashboard-all';
-
 const DashboardPage = () => {
-  return (
-    <APageBox title="Collection" showTitle={false}>
-      <DashboardAll />
-    </APageBox>
-  );
+  return null;
 };
 
 export default DashboardPage;
