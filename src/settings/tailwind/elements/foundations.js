d74aef72949e0eb5babb0fc6d01e1e48bb8a0f96
nneverlander <findadit@gmail.com>
2023-01-08 19:35:10 -0300

    adds new fonts, deletes old fonts

--- src/settings/tailwind/elements/foundations.js
+++ src/settings/tailwind/elements/foundations.js
@@ -107,9 +107,9 @@ module.exports = {
     'blur-100': 'blur(100px)'
   },
   fontFamily: {
-    body: 'F37 Bolton,SF UI Text,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
+    body: 'DM Sans,SF UI Text,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
     heading:
-      'F37 Zagma Mono, SF UI Display,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
+      'Kollektif,SF UI Display,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
     monospace: 'SF Mono,Monaco,Inconsolata,Fira Mono,Droid Sans Mono,Source Code Pro,monospace'
   },
   boxShadow: {
