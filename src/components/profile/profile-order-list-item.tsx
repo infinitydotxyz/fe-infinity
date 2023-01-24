6c3b0a5af7199b85306366e6048fd930c5b22ff2
nneverlander <findadit@gmail.com>
2023-01-24 13:30:21 -0300

    deletes lower price modal

--- src/components/profile/profile-order-list-item.tsx
+++ src/components/profile/profile-order-list-item.tsx
@@ -6,7 +6,6 @@ import { ERC721OrderCartItem, TokensFilter } from 'src/utils/types';
 import { secondaryTextColor, standardBorderCard } from 'src/utils/ui-constants';
 import { twMerge } from 'tailwind-merge';
 import { format } from 'timeago.js';
-import { ALowerPriceModal } from '../astra/modals/astra-lower-price-modal';
 import { OrderbookItem } from '../orderbook/list/orderbook-item';
 import { OrderDetailModal } from '../orderbook/order-detail-modal';
 
@@ -19,27 +18,11 @@ interface Props {
 
 export const ProfileOrderListItem = ({ order, orderType, onClickActionBtn, selected }: Props) => {
   const [selectedOrder, setSelectedOrder] = useState<SignedOBOrder | null>(null);
-  const [showLowerPriceModal, setShowLowerPriceModal] = useState(false);
-  const [startPriceEth, setStartPriceEth] = useState(order.startPriceEth);
+  const [startPriceEth] = useState(order.startPriceEth);
   const { checkSignedIn } = useOnboardContext();
-
-  const onClickLowerPrice = () => {
-    if (!checkSignedIn()) {
-      return;
-    }
-    setShowLowerPriceModal(true);
-  };
 
   return (
     <div>
-      {showLowerPriceModal && (
-        <ALowerPriceModal
-          isOpen={showLowerPriceModal}
-          onClose={() => setShowLowerPriceModal(false)}
-          order={order}
-          onDone={(val) => setStartPriceEth(val)}
-        />
-      )}
       <div className={twMerge(standardBorderCard, 'flex mx-4 text-sm')}>
         <div className="flex justify-between items-center w-full">
           <div className="w-1/4">
@@ -71,10 +54,12 @@ export const ProfileOrderListItem = ({ order, orderType, onClickActionBtn, selec
           {orderType === 'listings' || orderType === 'offers-made' ? (
             <Button
               onClick={() => {
-                onClickLowerPrice();
+                if (!checkSignedIn()) {
+                  return;
+                }
               }}
             >
-              Lower Price
+              Edit in Cart
             </Button>
           ) : null}
           {orderType === 'listings' || orderType === 'offers-made' ? (
