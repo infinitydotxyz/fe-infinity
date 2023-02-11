e3e1aa0139fca327137033c8226ec51eba4deca0
nneverlander <findadit@gmail.com>
2023-02-11 20:39:03 -0300

    setting swcMinify to false to prevent deploed version from throwing error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false, // running prod build fails with swcMinify: true
   reactStrictMode: true,
   distDir: process.env.NODE_ENV === 'development' ? 'build-dev' : 'build',
   // eslint-disable-next-line require-await
