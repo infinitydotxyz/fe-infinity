3a23e2a7b0e09250fb3dcb3dab1233541d44639d
nneverlander <findadit@gmail.com>
2023-02-22 13:46:52 -0300

    minor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,30 @@
+import { CenteredContent } from 'src/components/common';
+import { secondaryTextColor } from 'src/utils/ui-constants';
+import { twMerge } from 'tailwind-merge';
+
 const HomePage = () => {
-  return <div>Hi</div>;
+  return (
+    <div className="h-screen">
+      <CenteredContent>
+        <div className="space-y-4">
+          <div className="flex justify-center text-6xl font-heading font-bold mb-10">Flow</div>
+          <div>"Flow is the baddest marketplace on the planet" - Milk Road</div>
+          <div>"Flow has the meanest listing sniper in the game" - Metaversal</div>
+          <div>"Flow is the best place to bid on NFTs" - nftnow</div>
+          <div>"The world's only marketplace with auto executing orderbook" - Arthur Hayes</div>
+          <div>"These chads figured out vampired liquidity without incentives" - Chef Nomi</div>
+          <div>"Flow, the blockchain for open worlds" - Roham Gharegozlou</div>
+          <div className={twMerge('text-xs font-medium italic', secondaryTextColor)}>
+            *These were not said but what is said is true.
+          </div>
+          <div className={twMerge('text-xs font-medium italic', secondaryTextColor)}>
+            **Except for the last one. That was said. But flow.so ≠ flow.com. We might fuck around and build an NFT
+            chain though.
+          </div>
+        </div>
+      </CenteredContent>
+    </div>
+  );
 };
 
 export default HomePage;
