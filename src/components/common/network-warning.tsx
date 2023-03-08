49974ed2af1f3d47c7fd6036807beed0eef31231
nneverlander <findadit@gmail.com>
2023-03-08 14:20:24 -0500

    minor

--- src/components/common/network-warning.tsx
+++ src/components/common/network-warning.tsx
@@ -2,7 +2,7 @@ import { twMerge } from 'tailwind-merge';
 
 export const NetworkWarning = () => {
   return (
-    <div className={twMerge('flex justify-center px-4 py-2 text-sm text-white bg-red-600')}>
+    <div className={twMerge('flex justify-center px-4 py-2 text-sm text-white bg-yellow-600')}>
       Network mismatch. Make sure the chain your wallet is connected to is the same as the chain selected in the navbar.
     </div>
   );
