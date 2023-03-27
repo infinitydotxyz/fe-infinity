8c5ef7ed17bf7c3069b8253b22cf0f55730e18c1
nneverlander <findadit@gmail.com>
2023-03-27 09:54:32 -0500

    new logo and a few fixes

--- src/components/astra/astra-navbar.tsx
+++ src/components/astra/astra-navbar.tsx
@@ -3,7 +3,8 @@ import { borderColor } from 'src/utils/ui-constants';
 import { twMerge } from 'tailwind-merge';
 import { ConnectButton, EZImage, NextLink, Spacer } from 'src/components/common';
 import { CollectionSearchInput } from '../common/search/collection-search-input';
-import flowLogo from 'src/images/flow-logo.png';
+import flowLogoDark from 'src/images/flow-logo-dark.svg';
+import flowLogoLight from 'src/images/flow-logo-light.svg';
 import { useState } from 'react';
 import { ADropdown } from './astra-dropdown';
 import { useAppContext } from 'src/utils/context/AppContext';
@@ -11,18 +12,26 @@ import { chainIdToName } from 'src/utils';
 import { ChainId } from '@infinityxyz/lib-frontend/types/core';
 import { useNetwork } from 'wagmi';
 import { NetworkWarning } from '../common/network-warning';
+import { useTheme } from 'next-themes';
 
 export const ANavbar = () => {
   const { selectedChain, setSelectedChain } = useAppContext();
   const { chain } = useNetwork();
   const chainId = String(chain?.id);
   const [labelVal, setLabelVal] = useState(chainIdToName(selectedChain));
+  const { theme } = useTheme();
+  const darkMode = theme === 'dark';
+
   return (
     <div>
       {chain ? chainId !== selectedChain ? <NetworkWarning /> : null : null}
       <div className={twMerge('flex px-6 py-2 space-x-4 items-center border-b-[1px]', borderColor)}>
         <NextLink href="/trending">
-          <EZImage src={flowLogo.src} className="w-9 h-9" />
+          {darkMode ? (
+            <EZImage src={flowLogoDark.src} className="w-28 h-9" />
+          ) : (
+            <EZImage src={flowLogoLight.src} className="w-28 h-9" />
+          )}
         </NextLink>
 
         <div className="w-1/3">
