c87338dd6a9770e6454f474a43e88d867d1c3e8c
nneverlander <findadit@gmail.com>
2023-03-30 16:46:27 -0500

    fixes user profile bug

--- pages/profile/[address]/index.tsx
+++ pages/profile/[address]/index.tsx
@@ -1,5 +1,6 @@
 import { trimLowerCase } from '@infinityxyz/lib-frontend/utils';
 import { useRouter } from 'next/router';
+import { ReactNode, useEffect, useState } from 'react';
 import { CenteredContent, ConnectButton } from 'src/components/common';
 import { ProfileNFTs } from 'src/components/profile/profile-nfts';
 import { ProfileOrderList } from 'src/components/profile/profile-order-list';
@@ -20,30 +21,45 @@ export default function ProfileItemsPage() {
   const expanded = true;
   const tabs = [ProfileTabs.Items.toString(), ProfileTabs.Orders.toString()];
   const { selectedProfileTab } = useAppContext();
-
   const router = useRouter();
-  const addressFromPath = router.query.address as string;
-  if (!addressFromPath || addressFromPath === 'undefined') {
-    typeof window !== 'undefined' ? router.replace('/profile', undefined, { shallow: true }) : null;
-    return (
-      <CenteredContent>
-        <ConnectButton />
-      </CenteredContent>
-    );
-  }
 
-  if (trimLowerCase(addressFromPath) === trimLowerCase(address)) {
-    tabs.push(ProfileTabs.Send.toString());
-  }
-
-  return (
-    <div className="flex flex-col h-full w-full">
-      <ProfilePageHeader expanded={expanded} tabs={tabs} />
-      <div ref={setRef} className="overflow-y-auto scrollbar-hide">
-        {selectedProfileTab === ProfileTabs.Items && <ProfileNFTs userAddress={addressFromPath} />}
-        {selectedProfileTab === ProfileTabs.Orders && <ProfileOrderList userAddress={addressFromPath} />}
-        {selectedProfileTab === ProfileTabs.Send && <ProfileNFTs userAddress={addressFromPath} />}
-      </div>
-    </div>
+  const [content, setContent] = useState<ReactNode>(
+    <CenteredContent>
+      <ConnectButton />
+    </CenteredContent>
   );
+
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+
+    const addressFromPath = router.query.address as string;
+
+    if (!addressFromPath || addressFromPath === 'undefined') {
+      typeof window !== 'undefined' ? router.replace('/profile', undefined, { shallow: true }) : null;
+    } else {
+      if (trimLowerCase(addressFromPath) === trimLowerCase(address)) {
+        tabs.push(ProfileTabs.Send.toString());
+      }
+      setContent(
+        <div className="flex flex-col h-full w-full">
+          <ProfilePageHeader expanded={expanded} tabs={tabs} />
+          <div ref={setRef} className="overflow-y-auto scrollbar-hide">
+            {selectedProfileTab === ProfileTabs.Items && (
+              <ProfileNFTs userAddress={addressFromPath} key={addressFromPath} />
+            )}
+            {selectedProfileTab === ProfileTabs.Orders && (
+              <ProfileOrderList userAddress={addressFromPath} key={addressFromPath} />
+            )}
+            {selectedProfileTab === ProfileTabs.Send && (
+              <ProfileNFTs userAddress={addressFromPath} key={addressFromPath} />
+            )}
+          </div>
+        </div>
+      );
+    }
+  }, [router.isReady, router.asPath, selectedProfileTab]);
+
+  return content;
 }
