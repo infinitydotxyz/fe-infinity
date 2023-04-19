152c92d7dabb5b88619cbe3f6279e2d2b6374731
nneverlander <findadit@gmail.com>
2023-04-19 10:00:41 -0500

    minor changes to sing up flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ const BetaPage = () => {
     <div className="h-full w-full m-6">
       <div className="w-full h-full flex flex-col m-auto text-center justify-center items-center">
         <div className="w-max h-max flex flex-col justify-center p-8">
-          <div className="w-full my-4">
-            <h2 className="text-2xl font-heading font-bold center w-full">Sign Up</h2>
+          <div className="w-full my-2">
+            <h2 className="center w-full">Flow is currently invite only. Please complete sign up to get access.</h2>
           </div>
           <div>
             <SignUpFlow />
