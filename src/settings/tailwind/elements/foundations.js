693af499a1b5e5c1f9b52378dc0cdb997fab1d86
nneverlander <findadit@gmail.com>
2023-07-14 14:20:18 -0500

    deletes unused fonts

--- src/settings/tailwind/elements/foundations.js
+++ src/settings/tailwind/elements/foundations.js
@@ -110,8 +110,6 @@ module.exports = {
     body: 'PPNeueMachinaInktrap,DM Sans,SF UI Text,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
     heading:
       'Monument-Bold,SF UI Display,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
-    headingRegular:
-      'Monument-Regular,SF UI Display,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
     monospace: 'SF Mono,Monaco,Inconsolata,Fira Mono,Droid Sans Mono,Source Code Pro,monospace'
   },
   boxShadow: {
